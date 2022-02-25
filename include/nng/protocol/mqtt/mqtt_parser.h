3a50ade04e0fbc5416cb5613fffe16fc0e1431bb
alvin1221 <alvin@emqx.io>
2022-02-25 10:34:12 +0800

    * MDF [codec] add mqtt protocol version argument to nano_msg_composer().

--- include/nng/protocol/mqtt/mqtt_parser.h
+++ include/nng/protocol/mqtt/mqtt_parser.h
@@ -76,7 +76,7 @@ NNG_DECL uint8_t  verify_connect(conn_param *cparam, conf *conf);
 // repack
 NNG_DECL void nano_msg_set_dup(nng_msg *msg);
 NNG_DECL nng_msg *nano_msg_composer(nng_msg **, uint8_t retain, uint8_t qos,
-    mqtt_string *payload, mqtt_string *topic);
+    mqtt_string *payload, mqtt_string *topic, uint8_t proto_ver);
 NNG_DECL nng_msg *nano_msg_notify_disconnect(conn_param *cparam, uint8_t code);
 NNG_DECL nng_msg *nano_msg_notify_connect(conn_param *cparam, uint8_t code);
 NNG_DECL nano_pipe_db *nano_msg_get_subtopic(
