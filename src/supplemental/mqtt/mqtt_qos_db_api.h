1092edcfbd78e05f26eb4b8027a84d3820f94588
alvin1221 <alvin@emqx.io>
2022-03-23 18:16:03 +0800

    * NEW [qos_db] add nni_db_ops struct & related API functions.

---
+++ src/supplemental/mqtt/mqtt_qos_db_api.h
@@ -0,0 +1,20 @@
+#ifndef NNG_MQTT_QOS_DB_API_H
+#define NNG_MQTT_QOS_DB_API_H
+
+#include "core/nng_impl.h"
+#include "mqtt_qos_db.h"
+
+struct nni_db_ops {
+	void *db;
+	void (*db_init)(void **db);
+	void (*db_fini)(void *db);
+	void *(*db_get)(void *db, uint32_t);
+	void (*db_set)(void *db, uint32_t, void *);
+	void (*db_remove)(void *db, uint32_t);
+	void (*db_check_remove_msg)(void *db, void *);
+	void (*db_foreach)(void *db, void (*cb)(void *, void *));
+};
+
+typedef struct nni_db_ops nni_db_ops;
+
+#endif
