fdea6734c0d2451c47c340b496c0d25eea726a03
alvin1221 <alvin@emqx.io>
2022-03-23 11:18:29 +0800

    * MDF [qos_db] Add nni_time to be serialized and deserialized.

--- src/supplemental/mqtt/mqtt_qos_db_test.c
+++ src/supplemental/mqtt/mqtt_qos_db_test.c
@@ -20,13 +20,15 @@ test_qos_db_set(void)
 	sqlite3 *db = NULL;
 	nni_mqtt_qos_db_init(&db, DB_NAME);
 
-	char *header = "header002";
-	char *body   = "data002";
+	char *   header = "header002";
+	char *   body   = "data002";
+	nni_time ts     = 1648004331;
 
 	nni_msg *msg;
 	nni_msg_alloc(&msg, 0);
 	nni_msg_header_append(msg, header, strlen(header));
 	nni_msg_append(msg, body, strlen(body));
+	nni_msg_set_timestamp(msg, ts);
 
 	uint32_t pipe_id = 1000;
 
@@ -41,8 +43,9 @@ test_qos_db_get(void)
 	sqlite3 *db = NULL;
 	nni_mqtt_qos_db_init(&db, DB_NAME);
 
-	char *header = "header002";
-	char *body   = "data002";
+	char *   header = "header002";
+	char *   body   = "data002";
+	nni_time ts     = 1648004331;
 
 	uint32_t pipe_id = 1000;
 
@@ -51,6 +54,7 @@ test_qos_db_get(void)
 	TEST_CHECK(strncmp(header, nni_msg_header(msg),
 	               nni_msg_header_len(msg)) == 0);
 	TEST_CHECK(strncmp(body, nni_msg_body(msg), nni_msg_len(msg)) == 0);
+	TEST_CHECK(nni_msg_get_timestamp(msg) == ts);
 
 	nni_msg_free(msg);
 	nni_mqtt_qos_db_close(db);
@@ -80,9 +84,8 @@ test_qos_db_check_remove_msg(void)
 	nni_msg_alloc(&msg, 0);
 	nni_msg_header_append(msg, header, strlen(header));
 	nni_msg_append(msg, body, strlen(body));
-	
-	TEST_CHECK(
-	    nni_mqtt_qos_db_check_remove_msg(db, msg) == SQLITE_OK);
+
+	TEST_CHECK(nni_mqtt_qos_db_check_remove_msg(db, msg) == SQLITE_OK);
 
 	nni_msg_free(msg);
 	nni_mqtt_qos_db_close(db);
