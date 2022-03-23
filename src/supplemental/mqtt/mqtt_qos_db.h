061cb025494d006f42c114b90961ae3f674f6d1a
alvin1221 <alvin@emqx.io>
2022-03-23 18:17:26 +0800

    * MDF [qos_db] change functions' return type to void.

--- src/supplemental/mqtt/mqtt_qos_db.h
+++ src/supplemental/mqtt/mqtt_qos_db.h
@@ -32,19 +32,26 @@
 -----------------------
 **/
 
-NNG_DECL int nni_mqtt_qos_db_init(sqlite3 **, const char *);
-NNG_DECL int nni_mqtt_qos_db_close(sqlite3 *);
-NNG_DECL int nni_mqtt_qos_db_set(sqlite3 *, uint32_t, uint8_t, nni_msg *);
-NNG_DECL nni_msg *nni_mqtt_qos_db_get(sqlite3 *, uint32_t);
-NNG_DECL int      nni_mqtt_qos_db_remove(sqlite3 *, uint32_t);
+#define MQTT_DB_GET_QOS_BITS(msg) ((size_t) (msg) &0x03)
+#define MQTT_DB_PACKED_MSG_QOS(msg, qos) \
+	((nni_msg *) ((size_t) (msg) | ((qos) &0x03)))
 
-NNG_DECL int nni_mqtt_qos_db_set_pipe(sqlite3 *, uint32_t, const char *);
-NNG_DECL int nni_mqtt_qos_db_insert_pipe(sqlite3 *, uint32_t, const char *);
+#define MQTT_DB_GET_MSG_POINTER(msg) ((nni_msg *) ((size_t) (msg) & (~0x03)))
+
+NNG_DECL void nni_mqtt_qos_db_init(sqlite3 **);
+NNG_DECL void nni_mqtt_qos_db_close(sqlite3 *);
+NNG_DECL void nni_mqtt_qos_db_set(sqlite3 *, uint32_t, nni_msg *);
+NNG_DECL nni_msg *nni_mqtt_qos_db_get(sqlite3 *, uint32_t);
+NNG_DECL void     nni_mqtt_qos_db_remove(sqlite3 *, uint32_t);
+NNG_DECL void nni_mqtt_qos_db_foreach(sqlite3 *db, void (*cb)(void *, void *));
+NNG_DECL void nni_mqtt_qos_db_set_pipe(sqlite3 *, uint32_t, const char *);
+NNG_DECL void nni_mqtt_qos_db_insert_pipe(sqlite3 *, uint32_t, const char *);
 NNG_DECL
-int nni_mqtt_qos_db_remove_pipe(sqlite3 *, uint32_t);
+void nni_mqtt_qos_db_remove_pipe(sqlite3 *, uint32_t);
 NNG_DECL
-int nni_mqtt_qos_db_update_pipe_by_clientid(sqlite3 *, uint32_t, const char *);
-NNG_DECL int nni_mqtt_qos_db_update_all_pipe(sqlite3 *, uint32_t);
-NNG_DECL int nni_mqtt_qos_db_check_remove_msg(sqlite3 *, nni_msg *);
+void nni_mqtt_qos_db_update_pipe_by_clientid(
+    sqlite3 *, uint32_t, const char *);
+NNG_DECL void nni_mqtt_qos_db_update_all_pipe(sqlite3 *, uint32_t);
+NNG_DECL void nni_mqtt_qos_db_check_remove_msg(sqlite3 *, nni_msg *);
 
 #endif
