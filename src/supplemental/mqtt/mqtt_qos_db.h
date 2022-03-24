9ea518bad47e7e701ef29d0ad2164b11733da5c5
alvin1221 <alvin@emqx.io>
2022-03-24 11:43:47 +0800

    * MDF [qos_db] expose APIs.

--- src/supplemental/mqtt/mqtt_qos_db.h
+++ src/supplemental/mqtt/mqtt_qos_db.h
@@ -44,20 +44,18 @@
 
 #define MQTT_DB_GET_MSG_POINTER(msg) ((nni_msg *) ((size_t) (msg) & (~0x03)))
 
-NNG_DECL void nni_mqtt_qos_db_init(sqlite3 **);
-NNG_DECL void nni_mqtt_qos_db_close(sqlite3 *);
-NNG_DECL void nni_mqtt_qos_db_set(sqlite3 *, uint32_t, nni_msg *);
-NNG_DECL nni_msg *nni_mqtt_qos_db_get(sqlite3 *, uint32_t);
-NNG_DECL void     nni_mqtt_qos_db_remove(sqlite3 *, uint32_t);
-NNG_DECL void     nni_mqtt_qos_db_foreach(sqlite3 *, nni_idhash_cb);
-NNG_DECL void     nni_mqtt_qos_db_set_pipe(sqlite3 *, uint32_t, const char *);
-NNG_DECL void nni_mqtt_qos_db_insert_pipe(sqlite3 *, uint32_t, const char *);
-NNG_DECL
-void nni_mqtt_qos_db_remove_pipe(sqlite3 *, uint32_t);
-NNG_DECL
-void nni_mqtt_qos_db_update_pipe_by_clientid(
-    sqlite3 *, uint32_t, const char *);
-NNG_DECL void nni_mqtt_qos_db_update_all_pipe(sqlite3 *, uint32_t);
-NNG_DECL void nni_mqtt_qos_db_check_remove_msg(sqlite3 *, nni_msg *);
+extern void     nni_mqtt_qos_db_init(sqlite3 **);
+extern void     nni_mqtt_qos_db_close(sqlite3 *);
+extern void     nni_mqtt_qos_db_set(sqlite3 *, uint32_t, nni_msg *);
+extern nni_msg *nni_mqtt_qos_db_get(sqlite3 *, uint32_t);
+extern void     nni_mqtt_qos_db_remove(sqlite3 *, uint32_t);
+extern void     nni_mqtt_qos_db_foreach(sqlite3 *, nni_idhash_cb);
+extern void     nni_mqtt_qos_db_set_pipe(sqlite3 *, uint32_t, const char *);
+extern void     nni_mqtt_qos_db_insert_pipe(sqlite3 *, uint32_t, const char *);
+extern void     nni_mqtt_qos_db_remove_pipe(sqlite3 *, uint32_t);
+extern void     nni_mqtt_qos_db_update_pipe_by_clientid(
+        sqlite3 *, uint32_t, const char *);
+extern void nni_mqtt_qos_db_update_all_pipe(sqlite3 *, uint32_t);
+extern void nni_mqtt_qos_db_check_remove_msg(sqlite3 *, nni_msg *);
 
 #endif
