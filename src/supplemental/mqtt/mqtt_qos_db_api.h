1e111393f5be5b61f3df505c526deb79d0fa6480
alvin1221 <alvin@emqx.io>
2022-03-25 15:23:29 +0800

    * MDF [db_api] use MARCO replace qos_db_api.

--- src/supplemental/mqtt/mqtt_qos_db_api.h
+++ src/supplemental/mqtt/mqtt_qos_db_api.h
@@ -4,24 +4,33 @@
 #include "core/nng_impl.h"
 #include "mqtt_qos_db.h"
 
-struct nni_db_ops {
-	void *db;
-	void (*db_init)(void **db);
-	void (*db_fini)(void *db);
-	void *(*db_get)(void *db, uint32_t);
-	void (*db_set)(void *db, uint32_t, void *);
-	void (*db_remove)(void *db, uint32_t);
-	void (*db_check_remove_msg)(void *db, void *);
-	void (*db_foreach)(void *db, nni_idhash_cb);
-};
+#ifdef NNG_SUPP_SQLITE 
 
-typedef struct nni_db_ops nni_db_ops;
+#define nni_qos_db_init(db)  nni_mqtt_qos_db_init(&db)
+#define nni_qos_db_fini(db) nni_mqtt_qos_db_close(db)
+#define nni_qos_db_set(db, pipe_id, msg) nni_mqtt_qos_db_get(db, pipe_id, msg)
+#define nni_qos_db_get(db, pipe_id ) nni_mqtt_qos_db_set(db, pipe_id )
+#define nni_qos_db_remove(db, pipe_id) nni_mqtt_qos_db_remove(db, pipe_id) 
+#define nni_qos_db_foreach(db, cb) nni_mqtt_qos_db_foreach(db, cb)
+#define nni_qos_db_check_remove_msg(db, msg) nni_mqtt_qos_db_check_remove_msg(db, msg)
 
-extern void  nni_db_init(nni_db_ops *);
-extern void  nni_db_fini(nni_db_ops *);
-extern void  nni_db_set(nni_db_ops *, uint32_t, void *);
-extern void *nni_db_get(nni_db_ops *, uint32_t);
-extern void  nni_db_remove(nni_db_ops *, uint32_t);
-extern void  nni_db_check_remove_msg(nni_db_ops *, void *);
+#else
+
+#define nni_qos_db_init(db)                                             \
+	{                                                           \
+		(nni_id_map *) db = nng_zalloc(sizeof(nni_id_map)); \
+		nni_id_map_init(db, 0, 0, false);                   \
+	}
+
+#define nni_qos_db_fini(db) nni_id_map_fini(db)
+#define nni_qos_db_set(db, pipe_id, msg) nni_id_set(db, pipe_id, msg)
+#define nni_qos_db_get(db, pipe_id) nni_id_get(db, pipe_id)
+#define nni_qos_db_remove(db, pipe_id) nni_id_remove(db, pipe_id)
+#define nni_qos_db_foreach(db, cb) nni_id_map_foreach(db, cb)
+#define nni_qos_db_check_remove_msg(db, msg) nni_msg_free(msg)
+
+#endif
+
+
 
 #endif
