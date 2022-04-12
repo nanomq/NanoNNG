4d71e40165000edf887e88469077775917f8c410
Jaylin <jaylin@emqx.io>
2022-04-12 12:19:32 +0800

    * MDF [mqtt_parser] correct the func declartion

--- include/nng/nng.h
+++ include/nng/nng.h
@@ -1355,8 +1355,8 @@ NNG_DECL uint16_t       conn_param_get_keepalive(conn_param *cparam);
 NNG_DECL uint8_t        conn_param_get_protover(conn_param *cparam);
 NNG_DECL void *         conn_param_get_qos_db(conn_param *cparam);
 NNG_DECL void           conn_param_set_qos_db(conn_param *cparam, void *);
-NNG_DECL uint64_t conn_param_get_will_delay_timestamp(conn_param *cparam);
-NNG_DECL uint64_t conn_param_get_will_msg_expiry_timestamp(conn_param *cparam);
+NNG_DECL uint64_t conn_param_get_will_delay_interval(conn_param *cparam);
+NNG_DECL uint64_t conn_param_get_will_msg_expiry_interval(conn_param *cparam);
 
 NNG_DECL void nng_taskq_setter(int num_taskq_threads, int max_taskq_threads);
 #ifdef __cplusplus
