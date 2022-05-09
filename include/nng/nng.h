43aea3f8d66b22d31c265f748937bd8be998227f
Jaylin <jaylin@emqx.io>
2022-05-09 16:33:43 +0800

    * MDF [src/nng] re-enable nng msg property API for V5 retain

--- include/nng/nng.h
+++ include/nng/nng.h
@@ -1360,7 +1360,8 @@ NNG_DECL void          *conn_param_get_will_property(conn_param *cparam);
 NNG_DECL void           conn_param_set_qos_db(conn_param *cparam, void *);
 NNG_DECL uint64_t conn_param_get_will_delay_timestamp(conn_param *cparam);
 NNG_DECL uint64_t conn_param_get_will_mexp(conn_param *cparam);
-// NNG_DECL void     nng_msg_proto_set_property(nng_msg *m, void *p);
+NNG_DECL void     nng_msg_proto_set_property(nng_msg *m, void *p);
+NNG_DECL void *   nng_msg_proto_get_property(nng_msg *m);
 
 NNG_DECL void nng_taskq_setter(int num_taskq_threads, int max_taskq_threads);
 
