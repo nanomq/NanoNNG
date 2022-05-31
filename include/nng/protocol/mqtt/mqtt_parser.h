bf81ad07f17f365b05ae763fbf9d28f2e202824c
Jaylin <jaylin@emqx.io>
2022-05-31 16:09:31 +0800

    * MDF [mqtt_parser] rename nano_msg_composer to nano_pubmsg_composer

--- include/nng/protocol/mqtt/mqtt_parser.h
+++ include/nng/protocol/mqtt/mqtt_parser.h
@@ -83,7 +83,7 @@ NNG_DECL uint8_t  verify_connect(conn_param *cparam, conf *conf);
 
 // repack
 NNG_DECL void     nano_msg_set_dup(nng_msg *msg);
-NNG_DECL nng_msg *nano_msg_composer(nng_msg **, uint8_t retain, uint8_t qos,
+NNG_DECL nng_msg *nano_pubmsg_composer(nng_msg **, uint8_t retain, uint8_t qos,
     mqtt_string *payload, mqtt_string *topic, uint8_t proto_ver,
     nng_time time);
 NNG_DECL nng_msg *nano_msg_notify_disconnect(conn_param *cparam, uint8_t code);
