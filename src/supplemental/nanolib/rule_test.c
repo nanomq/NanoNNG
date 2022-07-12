670569da641e840a210f03fa294f1b5cb3c52981
lee-emqx <lihj@emqx.io>
2022-07-12 10:04:11 +0800

    * NEW [rule_engine] add rule_get_key_arr test.

--- src/supplemental/nanolib/rule_test.c
+++ src/supplemental/nanolib/rule_test.c
@@ -3,6 +3,7 @@
 #include "nng/nng.h"
 #include "nng/nng_debug.h"
 #include "core/nng_impl.h"
+#include "string.h"
 #include <nuts.h>
 
 
@@ -53,7 +54,36 @@ void test_rule_find_key(void)
 
 void test_rule_get_key_arr(void)
 {
-	
+
+	rule_key rk = { 0 };
+
+	char test_arr[][16] = {
+		".a",
+		".a.b",
+		".a.b.c",
+	};
+
+	char *rst_arr[4][3] = {
+		{"a", NULL, NULL},
+		{"a", "b", NULL},
+		{"a", "b", "c"},
+		NULL
+	};
+
+	int i = 0;
+	for (;i < 3; i++) {
+		rk.key_arr = NULL;
+		rule_get_key_arr(test_arr[i], &rk);
+		int j = 0;
+		while(rst_arr[i][j]) {
+			NUTS_NULL(!rk.key_arr[j]);
+			NUTS_PASS(strcmp(rk.key_arr[j], rst_arr[i][j]));
+			nng_free(rk.key_arr[j], strlen(rk.key_arr[j]));
+			j++;
+		}
+		cvector_free(rk.key_arr);
+	}
+
 	NUTS_PASS(0);
 
 }
