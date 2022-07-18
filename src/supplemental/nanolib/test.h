f392c0a5d27794890e6efaee2c20b26d5c2b9926
lee-emqx <lihj@emqx.io>
2022-07-18 15:05:31 +0800

    * NEW [test] make dbtree_test compile and pass.

---
+++ src/supplemental/nanolib/test.h
@@ -0,0 +1,10 @@
+// #include "mqtt_db.h"
+#ifndef TEST_H
+
+#define TEST_NUM_THREADS 8
+#define TEST_QUE_SIZE 13
+#define TEST_MSG_SIZE 16
+#define TEST_ARRAY_SIZE 10
+#define TEST_LOOP 100
+
+#endif
