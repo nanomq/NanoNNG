c64d0b376f71316ec6e077ef2f3b83409e283d66
wanghaemq <wanghamax@gmail.com>
2022-07-18 19:05:28 +0800

    * NEW [mqttv5] Add property interface for properties in sub.

--- include/nng/mqtt/mqtt_client.h
+++ include/nng/mqtt/mqtt_client.h
@@ -511,6 +511,9 @@ NNG_DECL void      nng_mqtt_msg_set_suback_property(nng_msg *, property *);
 
 NNG_DECL void     nng_mqtt_msg_set_unsubscribe_topics(
         nng_msg *, nng_mqtt_topic *, uint32_t);
+NNG_DECL property *nng_mqtt_msg_get_subscribe_property(nng_msg *);
+NNG_DECL void      nng_mqtt_msg_set_subscribe_property(nng_msg *, property *);
+
 NNG_DECL nng_mqtt_topic *nng_mqtt_msg_get_unsubscribe_topics(
     nng_msg *, uint32_t *);
 NNG_DECL property *nng_mqtt_msg_get_unsubscribe_property(nng_msg *);
