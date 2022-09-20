d99a27d2e42c6cf6774b39a89a94472efeaaa2c2
lee-emqx <lihj@emqx.io>
2022-09-20 18:33:51 +0800

    * NEW [hocon] add path_expression parser.

--- src/supplemental/nanolib/hocon.c
+++ src/supplemental/nanolib/hocon.c
@@ -17,6 +17,64 @@ static char *skip_whitespace(char *str)
 
 }
 
+static cJSON *path_expression_core(cJSON *parent, cJSON *jso)
+{
+    char *str = jso->string;
+    char *p = jso->string;
+    char *p_a = jso->string + strlen(jso->string);
+    cJSON *jso_new = NULL;
+    jso = cJSON_GetObjectItem(jso, jso->string);
+    char t[128] = { 0 };
+    while (NULL != (p = strrchr(str, '.'))) {
+
+        // a.b.c: {object}
+        // c ==> create json object jso(c, jso)
+        *p = '_';
+        strncpy(t, p + 1, p_a - p);
+        cJSON_AddItemToObject(jso_new, t, jso);
+        memset(t, 0, 128);
+        // jso_new = json(c, jso)
+        jso = jso_new;
+        jso_new = NULL;
+        p_a = --p;
+    }
+
+    strncpy(t, str, p_a - str + 1);
+    cJSON_AddItemToObject(parent, t, jso);
+    memset(t, 0, 128);
+
+    return parent;
+}
+
+// {"bridge.sqlite":{"enable":false,"disk_cache_size":102400,"mounted_file_path":"/tmp/","flush_mem_threshold":100,"resend_interval":5000}}
+// {"bridge":{"sqlite":{"enable":false,"disk_cache_size":102400,"mounted_file_path":"/tmp/","flush_mem_threshold":100,"resend_interval":5000}}}
+
+// level-order traversal
+// find key bridge.sqlite 
+// create object sqlite with object value
+// insert object bridge with sqlite
+// delete bridge.sqlite
+
+static cJSON *path_expression(cJSON *jso)
+{
+    cJSON *parent = jso;
+    cJSON *child = jso->child;
+
+    while (child) {
+        if (NULL != child->string && NULL != strchr(child->string, '.')) {
+            split(parent, child);
+        }
+        if (child->child) {
+            path_expression(child);
+        }
+        child = child->next;
+    }
+
+    return jso;
+}
+
+
+
 cJSON *hocon_str_to_json(char *str)
 {
     if (NULL == str) {
@@ -85,7 +143,7 @@ cJSON *hocon_str_to_json(char *str)
 
     if ((jso = cJSON_Parse(new))) {
         if (cJSON_False != cJSON_IsInvalid(jso)) {
-            return jso;
+            return path_expression(jso);
         }
     }
 
