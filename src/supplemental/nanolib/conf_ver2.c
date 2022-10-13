59ad66076604a003168cb39404ea68bdb6896d62
lee-emqx <lihj@emqx.io>
2022-10-13 16:34:27 +0800

    * NEW [conf_ver2] add hocon_read_str_base function.

--- src/supplemental/nanolib/conf_ver2.c
+++ src/supplemental/nanolib/conf_ver2.c
@@ -8,3 +8,22 @@
 #include "nng/supplemental/nanolib/hocon.h"
 #include <ctype.h>
 #include <string.h>
+
+// Read json value into struct
+// use same struct fields and json keys
+#define hocon_read_str_base(structure, field, key, jso)          \
+	do {                                                     \
+		cJSON *jso_key = cJSON_GetObjectItem(jso, key);  \
+		if (NULL == jso_key) {                           \
+			log_error("Read config %s failed!", key); \
+			break;                                   \
+		}                                                \
+		switch (jso_key->type) {                         \
+		case cJSON_String:                               \
+			(structure)->field =                     \
+			    nng_strdup(jso_key->valuestring);    \
+			break;                                   \
+		default:                                         \
+			break;                                   \
+		}                                                \
+	} while (0);
