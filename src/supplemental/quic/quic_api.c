4de7ec6193805a109f4cbf5de851a82a7ab32338
Jaylin <jaylin@emqx.io>
2022-12-13 18:58:41 +0800

    * FIX [mqtt_quic] only stop AIO when pipe is already started

--- src/supplemental/quic/quic_api.c
+++ src/supplemental/quic/quic_api.c
@@ -1109,6 +1109,7 @@ quic_strm_close(void *arg)
 // 	return (-2);
 }
 
+// return value 0 : normal close -1 : not even started
 int
 quic_pipe_close(uint8_t *code)
 {
@@ -1120,6 +1121,8 @@ quic_pipe_close(uint8_t *code)
 		qstrm->closed = true;
 		log_warn("close the QUIC stream!");
 		MsQuic->StreamClose(qstrm->stream);
+	} else {
+		return -1;
 	}
 
 	// take care of aios
