8fa0fb887adca11fb7414480d33f8ca0c43a5613
alvin1221 <alvin@emqx.io>
2023-01-18 16:15:52 +0800

    * MDF [conf] declare properties_init functions to nanolib.h

--- src/supplemental/nanolib/nanolib.h
+++ src/supplemental/nanolib/nanolib.h
@@ -8,4 +8,8 @@ extern void conf_tls_destroy(conf_tls *tls);
 extern void conf_tls_parse(
     conf_tls *tls, const char *path, const char *prefix1, const char *prefix2);
 
+extern void conf_bridge_sub_properties_init(conf_bridge_sub_properties *prop);
+extern void conf_bridge_conn_properties_init(
+    conf_bridge_conn_properties *prop);
+
 #endif //NNG_SUPPLEMENTAL_NANOLIB_NANOLIB_H
