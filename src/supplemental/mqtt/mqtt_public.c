4a18ae3d86e71a4bf2119004b923172220876b1d
Jaylin <jaylin@emqx.io>
2023-04-01 12:49:12 +0800

    * FIX [mqtt_client] caculate length internally!

--- src/supplemental/mqtt/mqtt_public.c
+++ src/supplemental/mqtt/mqtt_public.c
@@ -621,7 +621,7 @@ nng_mqtt_topic_qos_array_create(size_t n)
  * set each fields of nng_mqtt_topic_qos of a Sub packet
  * *topic_qos: array of topic/qos pair
  * index: target topic/qos pair
- * *topic_name: subscription topic
+ * *topic_name: subscription topic, string with '\0'
  * qos : MQTT QoS
  * nocal: Subscription Options  no local
  * rap: Subscription Options    Retain as publish
@@ -630,9 +630,9 @@ nng_mqtt_topic_qos_array_create(size_t n)
 */
 void
 nng_mqtt_topic_qos_array_set(nng_mqtt_topic_qos *topic_qos, size_t index,
-    const char *topic_name, uint32_t len,uint8_t qos, uint8_t nolocal, uint8_t rap, uint8_t rh)
+    const char *topic_name, uint8_t qos, uint8_t nolocal, uint8_t rap, uint8_t rh)
 {
-	nni_mqtt_topic_qos_array_set(topic_qos, index, topic_name, len, qos, nolocal, rap, rh);
+	nni_mqtt_topic_qos_array_set(topic_qos, index, topic_name, strlen(topic_name), qos, nolocal, rap, rh);
 }
 
 void
