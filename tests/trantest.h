edac668c18b1f27726a5b20a26ca0d276fccc944
Hermann0222 <yukang.wei@emqx.io>
2023-04-04 19:00:20 +0800

    * FIX [mqtt-tcp test] fix building error in C99.

--- tests/trantest.h
+++ tests/trantest.h
@@ -15,6 +15,7 @@
 #include <nng/protocol/reqrep0/rep.h>
 #include <nng/protocol/reqrep0/req.h>
 #include <nng/supplemental/util/platform.h>
+#include "nng/protocol/mqtt/mqtt_parser.h"
 
 #include "convey.h"
 #include "core/nng_impl.h"
