a5268eafc1f8105330110587296b80aa9a1b825e
Hermann0222 <yukang.wei@emqx.io>
2023-04-21 17:13:57 +0800

    * NEW [trantest] add test for unsub msg.

--- tests/trantest.h
+++ tests/trantest.h
@@ -56,6 +56,46 @@ typedef struct nano_work {
 	void *sqlite_db;
 } nano_work;
 
+// MQTT Fixed header
+struct fixed_header {
+	// flag_bits
+	uint8_t retain : 1;
+	uint8_t qos : 2;
+	uint8_t dup : 1;
+	// packet_types
+	uint8_t packet_type : 4;
+	// remaining length
+	uint32_t remain_len;
+};
+
+// MQTT Variable header
+union variable_header {
+	struct {
+		uint16_t           packet_id;
+		struct mqtt_string topic_name;
+		property           *properties;
+		uint32_t           prop_len;
+	} publish;
+
+	struct {
+		uint16_t    packet_id;
+		reason_code reason_code;
+		property    *properties;
+		uint32_t    prop_len;
+	} pub_arrc, puback, pubrec, pubrel, pubcomp;
+};
+
+struct mqtt_payload {
+	uint8_t *data;
+	uint32_t len;
+};
+
+struct pub_packet_struct {
+	struct fixed_header   fixed_header;
+	union variable_header var_header;
+	struct mqtt_payload   payload;
+};
+
 // Transport common tests.  By making a common test framework for transports,
 // we can avoid rewriting the same tests for each new transport.  Include this
 // file once in your test code.  The test framework uses the REQ/REP protocol
@@ -562,19 +602,19 @@ trantest_mqtt_pub(nng_socket sock)
 	nng_mqtt_msg_set_publish_topic(pubmsg, params.topic);
 	So(nng_sendmsg(sock, pubmsg, NNG_FLAG_NONBLOCK) == 0);
 
-	conn_param *cp = NULL;
-	while (1) {
-		nng_msg *msg = NULL;
-		if (nng_recvmsg(sock, &msg, 0) != 0) {
-			continue;
-		}
-		if (nng_mqtt_msg_get_packet_type(msg) == NNG_MQTT_CONNACK) {
-			cp = nng_msg_get_conn_param(msg);
-			nng_msg_free(msg);
-			break;
-		}
-	}
-	conn_param_free(cp);
+	// conn_param *cp = NULL;
+	// while (1) {
+	// 	nng_msg *msg = NULL;
+	// 	if (nng_recvmsg(sock, &msg, 0) != 0) {
+	// 		continue;
+	// 	}
+	// 	if (nng_mqtt_msg_get_packet_type(msg) == NNG_MQTT_CONNACK) {
+	// 		cp = nng_msg_get_conn_param(msg);
+	// 		nng_msg_free(msg);
+	// 		break;
+	// 	}
+	// }
+	// conn_param_free(cp);
 }
 
 void
@@ -895,13 +935,159 @@ encode_suback_msg(nng_msg *msg, nano_work *work)
 	return 0;
 }
 
+int
+decode_unsub_msg(nano_work *work)
+{
+	uint8_t *variable_ptr;
+	uint8_t *payload_ptr;
+	uint32_t vpos = 0; // pos in variable
+	uint32_t bpos = 0; // pos in payload
+
+	uint32_t len_of_varint = 0, len_of_property = 0, len_of_properties = 0;
+	uint32_t len_of_str = 0, len_of_topic;
+
+	packet_unsubscribe *unsub_pkt     = work->unsub_pkt;
+	nng_msg *           msg           = work->msg;
+	size_t              remaining_len = nng_msg_remaining_len(msg);
+
+	uint8_t property_id;
+	topic_node *       tn, *_tn;
+
+	const uint8_t proto_ver = work->proto_ver;
+
+	// handle varibale header
+	variable_ptr = nng_msg_body(msg);
+	NNI_GET16(variable_ptr, unsub_pkt->packet_id);
+	vpos += 2;
+
+	unsub_pkt->properties = NULL;
+	unsub_pkt->prop_len = 0;
+	// Mqtt_v5 include property
+	unsub_pkt->properties = NULL;
+	if (MQTT_PROTOCOL_VERSION_v5 == proto_ver) {
+		unsub_pkt->properties =
+		    decode_properties(msg, &vpos, &unsub_pkt->prop_len, false);
+		if (check_properties(unsub_pkt->properties) != SUCCESS) {
+			return PROTOCOL_ERROR;
+		}
+	}
+
+	log_debug("remain_len: [%ld] packet_id : [%d]", remaining_len,
+	    unsub_pkt->packet_id);
+
+	// handle payload
+	payload_ptr = nng_msg_payload_ptr(msg);
+
+	if ((tn = nng_alloc(sizeof(topic_node))) == NULL) {
+		log_debug("nng_alloc");
+		return NNG_ENOMEM;
+	}
+	unsub_pkt->node = tn;
+	tn->next = NULL;
+
+	while (1) {
+		_tn = tn;
+
+		len_of_topic = get_utf8_str(&tn->topic.body, payload_ptr, &bpos);
+		if (len_of_topic != -1) {
+			tn->topic.len = len_of_topic;
+		} else {
+			tn->reason_code = UNSPECIFIED_ERROR;
+			log_debug("not utf-8 format string.");
+			return PROTOCOL_ERROR;
+		}
+
+		log_debug("bpos+vpos: [%d] remain_len: [%ld]", bpos + vpos,
+		    remaining_len);
+		if (bpos < remaining_len - vpos) {
+			if ((tn = nng_alloc(sizeof(topic_node))) == NULL) {
+				log_debug("nng_alloc");
+				return NNG_ENOMEM;
+			}
+			tn->next  = NULL;
+			_tn->next = tn;
+		} else {
+			break;
+		}
+	}
+	return 0;
+}
+
+int
+encode_unsuback_msg(nng_msg *msg, nano_work *work)
+{
+	nng_msg_header_clear(msg);
+	nng_msg_clear(msg);
+
+	uint8_t     packet_id[2];
+	uint8_t     varint[4];
+	uint8_t     reason_code, cmd, property_len = 0;
+	uint32_t    remaining_len;
+	int         len_of_varint, rv;
+	topic_node *tn;
+
+	packet_unsubscribe *unsub_pkt = work->unsub_pkt;
+	const uint8_t       proto_ver = work->proto_ver;
+
+	// handle variable header first
+	NNI_PUT16(packet_id, unsub_pkt->packet_id);
+	if ((rv = nng_msg_append(msg, packet_id, 2)) != 0) {
+		log_debug("nng_msg_append");
+		return PROTOCOL_ERROR;
+	}
+
+	if (MQTT_PROTOCOL_VERSION_v5 == proto_ver) {
+		//TODO set property if necessary 
+		encode_properties(msg, NULL, CMD_UNSUBACK);
+	}
+
+	// handle payload
+	// no payload in mqtt_v3
+	if (MQTT_PROTOCOL_VERSION_v5 == proto_ver) {
+		tn = unsub_pkt->node;
+		while (tn) {
+			reason_code = tn->reason_code;
+			if ((rv = nng_msg_append(
+			         msg, (uint8_t *) &reason_code, 1)) != 0) {
+				log_debug("nng_msg_append [%d]", rv);
+				return PROTOCOL_ERROR;
+			}
+			tn = tn->next;
+			log_debug("reason_code: [%x]", reason_code);
+		}
+	}
+
+	// handle fixed header
+	cmd = CMD_UNSUBACK;
+	if ((rv = nng_msg_header_append(msg, (uint8_t *) &cmd, 1)) != 0) {
+		log_debug("nng_msg_header_append [%d]", rv);
+		return PROTOCOL_ERROR;
+	}
+
+	remaining_len = (uint32_t) nng_msg_len(msg);
+	len_of_varint = put_var_integer(varint, remaining_len);
+	if ((rv = nng_msg_header_append(msg, varint, len_of_varint)) != 0) {
+		log_debug("nng_msg_header_append [%d]", rv);
+		return PROTOCOL_ERROR;
+	}
+
+	log_debug("unsuback:"
+	          " remain: [%d]"
+	          " varint: [%d %d %d %d]"
+	          " len: [%d]"
+	          " packet_id: [%x %x]",
+	    remaining_len, varint[0], varint[1], varint[2], varint[3],
+	    len_of_varint, packet_id[0], packet_id[1]);
+
+	return 0;
+}
 
 void
 trantest_mqtt_broker_send_recv(trantest *tt)
 {
 	Convey("mqtt broker pub and sub", {
 		const char      *url   = "mqtt-tcp://127.0.0.1:1883";
-		uint8_t          qos   = 2;
+		uint8_t          qos   = 0;
 		const char      *topic = "myTopic";
 		const char      *data  = "ping";
 		nng_dialer       dialer;
@@ -944,26 +1130,49 @@ trantest_mqtt_broker_send_recv(trantest *tt)
 		rcp = nng_msg_get_conn_param(msg);
 		// nng_msg_free(msg);
 
-		// client send sub & server send suback
+		// client send sub & server send suback.
 		transtest_mqtt_sub_send(tt->reqsock, client, true);
 		nng_msleep(100);
 		nng_ctx_recv(work->ctx, work->aio);
 		So((rmsg = nng_aio_get_msg(work->aio)) != NULL);
 		So(nng_msg_get_type(rmsg) == CMD_SUBSCRIBE);
+		// decode sub msg and encode suback msg.
 		So((work->sub_pkt = nng_alloc(sizeof(packet_subscribe))) != NULL);
 		memset(work->sub_pkt, '\0', sizeof(packet_subscribe));
 		work->msg = rmsg;
 		work->pid       = nng_msg_get_pipe(work->msg);
 		work->cparam    = nng_msg_get_conn_param(work->msg);
 		work->proto_ver = conn_param_get_protover(work->cparam);
-		decode_sub_msg(work);
-		encode_suback_msg(rmsg, work);
+		So(decode_sub_msg(work) == 0);
+		So(encode_suback_msg(rmsg, work) == 0);
 		// sub_pkt_free(work->sub_pkt);
 		nng_msg_set_cmd_type(rmsg, CMD_SUBACK);
 		nng_aio_set_msg(work->aio, rmsg);
 		nng_ctx_send(work->ctx, work->aio);
+
+		// client send pub & server send puback
+		trantest_mqtt_pub(tt->reqsock);
+		nng_msleep(100);
+		nng_ctx_recv(work->ctx, work->aio);
+		So((rmsg = nng_aio_get_msg(work->aio)) != NULL);
+		So(nng_msg_get_type(rmsg) == CMD_PUBLISH);
+		nng_msg_free(rmsg);
+
+		// client send unsub msg
+		transtest_mqtt_unsub_send(tt->reqsock, client, true);
+		nng_msleep(100);
+		nng_ctx_recv(work->ctx, work->aio);
+		So((rmsg = nng_aio_get_msg(work->aio)) != NULL);
+		So(nng_msg_get_type(rmsg) == CMD_UNSUBSCRIBE);
+		So((work->unsub_pkt = nng_alloc(sizeof(packet_unsubscribe))) != NULL);
+		work->msg = rmsg;
+		So(decode_unsub_msg(work) == 0);
+		So(encode_unsuback_msg(rmsg, work) == 0);
+		nng_aio_set_msg(work->aio, work->msg);
 		work->msg = NULL;
-		
+		nng_ctx_send(work->ctx, work->aio);
+		nng_aio_finish(work->aio, 0);
+
 		// nng_msleep(1000);
 		// printf("qqqqqqqqqqqqq\n");
 		// So(nng_recvmsg(tt->reqsock, &msg, 0) == 0);
