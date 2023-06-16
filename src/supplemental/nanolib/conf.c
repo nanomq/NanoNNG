2d84fa9942e21aa5cb44aaab14cfb03b46112eb9
jaylin <jaylin@emqx.io>
2023-06-16 15:37:25 +0800

    * MDF [conf] remove an redundunt code

--- src/supplemental/nanolib/conf.c
+++ src/supplemental/nanolib/conf.c
@@ -2421,7 +2421,6 @@ conf_bridge_node_init(conf_bridge_node *node)
 
 	node->bridge_aio     = NULL;
 	node->bridge_reload_aio = NULL;
-	node->bridge_reload_aio2 = NULL;
 	node->bridge_arg = NULL;
 
 #if defined(SUPP_QUIC)
