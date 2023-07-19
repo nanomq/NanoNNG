d687d02b7e277a00b2295b2727fdf7d965c204ee
wanghaemq <wangwei@emqx.io>
2023-07-19 16:54:06 +0800

    * ADD [msquic_dial] Add fields in nni quic dialer structure.

--- src/supplemental/quic/msquic_dial.c
+++ src/supplemental/quic/msquic_dial.c
@@ -9,6 +9,7 @@
 
 // #if defined(NNG_ENABLE_QUIC) // && defined(NNG_QUIC_MSQUIC)
 
+#include "core/defs.h"
 #include "quic_api.h"
 #include "core/nng_impl.h"
 #include "msquic.h"
@@ -48,14 +49,44 @@
 #include <unistd.h>
 
 struct nni_quic_dialer {
+	nni_aio                *qconaio; // for quic connection
+	nni_aio                *qstrmaio; // for quic stream
+	nni_list                connq;   // pending connections/quic streams
+	bool                    closed;
+	bool                    nodelay;
+	bool                    keepalive;
+	struct sockaddr_storage src;
+	size_t                  srclen;
+	nni_mtx                 mtx;
+	nni_atomic_u64          ref;
+	nni_atomic_bool         fini;
+
+	// MsQuic
+	HQUIC                   qconn; // quic connection
 };
 
-int
-nni_quic_dialer_init(void *arg)
-{
-	nni_quic_dialer *d = arg;
-	NNI_ARG_UNUSED(d);
+static void quic_dialer_cb();
+static void quic_dialer_strm_cb();
 
+int
+nni_quic_dialer_init(void **argp)
+{
+	nni_quic_dialer *d;
+
+	if ((d = NNI_ALLOC_STRUCT(d)) == NULL) {
+		return (NNG_ENOMEM);
+	}
+
+	nni_mtx_init(&d->mtx);
+	d->closed = false;
+	nni_aio_alloc(&d->qconaio, quic_dialer_cb, (void *)d);
+	nni_aio_alloc(&d->qconaio, quic_dialer_strm_cb, (void *)d);
+	nni_aio_list_init(&d->connq);
+	nni_atomic_init_bool(&d->fini);
+	nni_atomic_init64(&d->ref);
+	nni_atomic_inc64(&d->ref);
+
+	*argp = d;
 	return 0;
 }
 
