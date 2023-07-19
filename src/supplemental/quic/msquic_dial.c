2179495e462e5b14fd5ba1a21d10cd0f8ef50ba9
wanghaemq <wangwei@emqx.io>
2023-07-19 16:55:40 +0800

    * ADD [quic] Add nni quic dial supported.

--- src/supplemental/quic/msquic_dial.c
+++ src/supplemental/quic/msquic_dial.c
@@ -95,15 +95,120 @@ nni_quic_dialer_fini(void *arg)
 {
 	nni_quic_dialer *d = arg;
 	NNI_ARG_UNUSED(d);
-
-	return;
 }
 
+static void
+quic_dialer_strm_cancel(nni_aio *aio, void *arg, int rv)
+{
+	nni_tcp_dialer *d = arg;
+	nni_tcp_conn *  c;
+
+	nni_mtx_lock(&d->mtx);
+	if ((!nni_aio_list_active(aio)) ||
+	    ((c = nni_aio_get_prov_data(aio)) == NULL)) {
+		nni_mtx_unlock(&d->mtx);
+		return;
+	}
+	nni_aio_list_remove(aio);
+	c->dial_aio = NULL;
+	nni_aio_set_prov_data(aio, NULL);
+	nni_mtx_unlock(&d->mtx);
+
+	nni_aio_finish_error(aio, rv);
+	nng_stream_free(&c->stream);
+}
+
+static void
+quic_dialer_cancel(nni_aio *aio, void *arg, int rv)
+{
+	nni_tcp_dialer *d = arg;
+	nni_tcp_conn *  c;
+
+	nni_mtx_lock(&d->mtx);
+	if ((!nni_aio_list_active(aio)) ||
+	    ((c = nni_aio_get_prov_data(aio)) == NULL)) {
+		nni_mtx_unlock(&d->mtx);
+		return;
+	}
+	nni_aio_list_remove(aio);
+	c->dial_aio = NULL;
+	nni_aio_set_prov_data(aio, NULL);
+	nni_mtx_unlock(&d->mtx);
+
+	nni_aio_finish_error(aio, rv);
+	nng_stream_free(&c->stream);
+}
+
+static void
+quic_dialer_cb(void *arg)
+{
+	nni_quic_dialer *d = arg;
+
+	if (nni_aio_result(d->qconaio) != 0) {
+		return;
+	}
+
+	// Connection was established. Nice. Then. Create the main quic stream.
+	if ((rv = nni_aio_schedule(d->qstrmaio, quic_dialer_strm_cancel, d)) != 0) {
+		goto error;
+	}
+
+	msquic_strm_connect(d->qconn, d);
+}
+
+static void
+quic_dialer_strm_cb()
+{
+}
+
+// Dial to the `url`. Finish `aio` when connected.
+// For quic. If the connection is not established.
+// This nng stream is linked to main quic stream.
+// Or it will link to a sub quic stream.
 void
 nni_quic_dial(void *arg, const char *url, nni_aio *aio)
 {
 	nni_quic_dialer *d = arg;
-	NNI_ARG_UNUSED(d);
+	nni_quic_conn *  c;
+	bool             ismain = false;
+
+	if (nni_aio_begin(aio) != 0) {
+		return;
+	}
+
+	nni_atomic_inc64(&d->ref);
+
+	// Create a connection whenever dial. So it's okey. right?
+	if ((rv = nni_msquic_quic_alloc(&c, d)) != 0) {
+		nni_aio_finish_error(aio, rv);
+		nni_msquic_quic_dialer_rele(d);
+		return;
+	}
+
+	nni_mtx_lock(&d->mtx);
+	// TODO check if there are any quic streams to the url.
+	ismain = true;
+
+	if ((rv = nni_aio_schedule(aio, quic_dialer_cancel, d)) != 0) {
+		goto error;
+	}
+
+	// pass c to the qstrmaio for getting it in quic_dialer_strm_cb
+	nni_aio_set_prov_data(d->qstrmaio, c);
+
+	if (ismain) {
+		// Make a quic connection to the url.
+		// Create stream after connection is established.
+		msquic_connect(url, d);
+	} else {
+		msquic_strm_connect(d->qconn, d);
+	}
+
+	nni_mtx_unlock(&d->mtx);
+
+	return;
+error:
+
 }
 
 void
