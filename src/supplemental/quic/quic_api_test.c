0cfd94c8fe9a80d6b8cdf2064a0659491c0193a4
wanghaemq <wangwei@emqx.io>
2023-07-25 16:23:40 +0800

    * NEW [msquic_dial] Add unit test for sending msg in quic stream

--- src/supplemental/quic/quic_api_test.c
+++ src/supplemental/quic/quic_api_test.c
@@ -31,7 +31,36 @@ test_quic_conn_refused(void)
 void
 test_quic_large_message(void)
 {
-	return;
+	nng_stream_dialer *dialer;
+	nng_aio *          aio;
+	nng_stream *       s;
+	void *             t;
+	uint8_t *          buf;
+	size_t             size = 450001;
+
+	// allocate messages
+	NUTS_ASSERT((buf = nng_alloc(size)) != NULL);
+	for (size_t i = 0; i < size; i++) {
+		buf[i] = rand() & 0xff;
+	}
+
+	NUTS_PASS(nng_aio_alloc(&aio, NULL, NULL));
+	nng_aio_set_timeout(aio, 5000); // 5 sec
+
+	// port 8 is generally not used for anything.
+	NUTS_PASS(nng_stream_dialer_alloc(&dialer, "quic://127.0.0.1:14567"));
+	nng_stream_dialer_dial(dialer, aio);
+	nng_aio_wait(aio);
+	NUTS_PASS(nng_aio_result(aio));
+
+	NUTS_TRUE((s = nng_aio_get_output(aio, 0)) != NULL);
+	t = nuts_stream_send_start(s, buf, size);
+	NUTS_PASS(nuts_stream_wait(t));
+
+	// nng_free(buf, size);
+	nng_stream_free(s);
+	nng_stream_dialer_free(dialer);
+	nng_aio_free(aio);
 }
 
 TEST_LIST = {
