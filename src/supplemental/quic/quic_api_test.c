32ea4efd08ee623f10a470dbcea5286c3432f9eb
wanghaemq <wangwei@emqx.io>
2023-07-27 17:55:51 +0800

    * ADD [msquic_dial] Add quic mqtt connection test.

--- src/supplemental/quic/quic_api_test.c
+++ src/supplemental/quic/quic_api_test.c
@@ -47,7 +47,6 @@ test_quic_send_message(void)
 	NUTS_PASS(nng_aio_alloc(&aio, NULL, NULL));
 	nng_aio_set_timeout(aio, 5000); // 5 sec
 
-	// port 8 is generally not used for anything.
 	NUTS_PASS(nng_stream_dialer_alloc(&dialer, "quic://127.0.0.1:14567"));
 	nng_stream_dialer_dial(dialer, aio);
 	nng_aio_wait(aio);
@@ -89,7 +88,53 @@ test_quic_recv_message(void)
 	NUTS_PASS(nng_aio_alloc(&aio, NULL, NULL));
 	nng_aio_set_timeout(aio, 5000); // 5 sec
 
-	// port 8 is generally not used for anything.
+	NUTS_PASS(nng_stream_dialer_alloc(&dialer, "quic://127.0.0.1:14567"));
+	nng_stream_dialer_dial(dialer, aio);
+	nng_aio_wait(aio);
+	NUTS_PASS(nng_aio_result(aio));
+
+	NUTS_TRUE((s = nng_aio_get_output(aio, 0)) != NULL);
+
+	// Instead of quic echo server. We test a mqtt quic server.
+	// To get some data from mqtt quic server. We need to send a connect msg first.
+	t = nuts_stream_send_start(s, connect, sz_connect);
+	NUTS_PASS(nuts_stream_wait(t));
+
+	t = nuts_stream_recv_start(s, buf, size);
+	NUTS_PASS(nuts_stream_wait(t));
+
+	nng_free(buf, size);
+	nng_stream_free(s);
+	nng_stream_dialer_free(dialer);
+	nng_aio_free(aio);
+}
+
+void
+test_quic_mqtt_connection(void)
+{
+	nng_stream_dialer *dialer;
+	nng_aio *          aio;
+	nng_stream *       s;
+	void *             t;
+	uint8_t *          buf;
+	size_t             size = 4;
+
+	// allocate messages
+	NUTS_ASSERT((buf = nng_alloc(size)) != NULL);
+	uint8_t connect[] = {
+		0x10, 0x3f, 0x00, 0x04, 0x4d, 0x51, 0x54, 0x54, 0x04, 0xc6,
+		0x00, 0x3c, 0x00, 0x0c, 0x54, 0x65, 0x73, 0x74, 0x2d, 0x43,
+		0x6c, 0x69, 0x65, 0x6e, 0x74, 0x31, 0x00, 0x0a, 0x77, 0x69,
+		0x6c, 0x6c, 0x5f, 0x74, 0x6f, 0x70, 0x69, 0x63, 0x00, 0x07,
+		0x62, 0x79, 0x65, 0x2d, 0x62, 0x79, 0x65, 0x00, 0x05, 0x61,
+		0x6c, 0x76, 0x69, 0x6e, 0x00, 0x09, 0x48, 0x48, 0x48, 0x31,
+		0x32, 0x33, 0x34, 0x35, 0x36
+	};
+	size_t sz_connect = sizeof(connect) / sizeof(uint8_t);
+
+	NUTS_PASS(nng_aio_alloc(&aio, NULL, NULL));
+	nng_aio_set_timeout(aio, 5000); // 5 sec
+
 	NUTS_PASS(nng_stream_dialer_alloc(&dialer, "quic://127.0.0.1:14567"));
 	nng_stream_dialer_dial(dialer, aio);
 	nng_aio_wait(aio);
