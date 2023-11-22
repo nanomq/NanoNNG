5d56e24e440e27e7570910b4ba1660edb858f8b5
Moi Ran <maoyi.ran@emqx.io>
2023-11-22 16:18:12 +0800

    * ADD [ringbuffer] Export ringBuffer_add_rule
    Signed-off-by: Moi Ran <maoyi.ran@emqx.io>

--- include/nng/supplemental/nanolib/ringbuffer.h
+++ include/nng/supplemental/nanolib/ringbuffer.h
@@ -17,21 +17,45 @@ struct ringBufferMsg {
 };
 
 struct ringBuffer {
-	unsigned int       head;
-	unsigned int       tail;
-	unsigned int       size;
-	unsigned int       cap;
+	unsigned int          head;
+	unsigned int          tail;
+	unsigned int          size;
+	unsigned int          cap;
 	/* Whether to allow overwriting of old data when the queue is full */
-	unsigned int       overWrite;
+	unsigned int          overWrite;
 	/* TTL of all messages in ringbuffer */
-	unsigned long long expiredAt;
+	unsigned long long    expiredAt;
+	unsigned int          enqinRuleListLen;
+	unsigned int          enqoutRuleListLen;
+	unsigned int          deqinRuleListLen;
+	unsigned int          deqoutRuleListLen;
+	struct ringBufferRule *enqinRuleList[RBRULELIST_MAX_SIZE];
+	struct ringBufferRule *enqoutRuleList[RBRULELIST_MAX_SIZE];
+	struct ringBufferRule *deqinRuleList[RBRULELIST_MAX_SIZE];
+	struct ringBufferRule *deqoutRuleList[RBRULELIST_MAX_SIZE];
 
 	/* TODO: LOCK */
 
 	struct ringBufferMsg *msgs;
 };
 
-#define RINGBUFFER_MAX_SIZE 0xffff
+struct ringBufferRule {
+	/*
+	 * flag: ENQUEUE_IN_HOOK/ENQUEUE_OUT_HOOK/DEQUEUE_IN_HOOK/DEQUEUE_OUT_HOOK
+	 * return: 0: success, -1: failed
+	 */
+	int (*match)(struct ringBuffer *rb, void *data, int flag);
+	/*
+	 * return: 0: continue, -1: stop and return
+	 */
+	int (*target)(struct ringBuffer *rb, void *data, int flag);
+};
+
+
+int ringBuffer_add_rule(struct ringBuffer *rb,
+						int (*match)(struct ringBuffer *rb, void *data, int flag),
+						int (*target)(struct ringBuffer *rb, void *data, int flag),
+						int flag);
 
 int ringBuffer_init(struct ringBuffer **rb,
 					unsigned int cap,
