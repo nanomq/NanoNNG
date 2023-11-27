eb6c3bf581b0b5db50774b1adc39638ee2ef6a1e
Moi Ran <maoyi.ran@emqx.io>
2023-11-27 19:31:33 +0800

    * MDF [exchange_client] Send mqtt msg in exchange_client UT
    Signed-off-by: Moi Ran <maoyi.ran@emqx.io>

--- src/mqtt/protocol/exchange/exchange_client_test.c
+++ src/mqtt/protocol/exchange/exchange_client_test.c
@@ -5,24 +5,29 @@
 #include <nuts.h>
 
 #define UNUSED(x) ((void) x)
-
-static int match_times = 0;
-static int target_times = 0;
-
-static int
-match_cb(void *msg)
+//
+// Publish a message to the given topic and with the given QoS.
+int
+client_publish(nng_socket sock, const char *topic, uint8_t *payload,
+    uint32_t payload_len, uint8_t qos, bool verbose)
 {
-	UNUSED(msg);
-	match_times++;
-	return 0;
-}
+	int rv;
 
-static int
-target_cb(void *msg)
-{
-	UNUSED(msg);
-	target_times++;
-	return 0;
+	// create a PUBLISH message
+	nng_msg *pubmsg;
+	nng_mqtt_msg_alloc(&pubmsg, 0);
+	nng_mqtt_msg_set_packet_type(pubmsg, NNG_MQTT_PUBLISH);
+	nng_mqtt_msg_set_publish_dup(pubmsg, 0);
+	nng_mqtt_msg_set_publish_qos(pubmsg, qos);
+	nng_mqtt_msg_set_publish_retain(pubmsg, 0);
+	nng_mqtt_msg_set_publish_payload(
+	    pubmsg, (uint8_t *) payload, payload_len);
+	nng_mqtt_msg_set_publish_topic(pubmsg, topic);
+	nng_mqtt_msg_set_publish_topic_len(pubmsg, strlen(topic));
+
+	rv = nng_sendmsg(sock, pubmsg, NNG_FLAG_NONBLOCK);
+
+	return rv;
 }
 
 void
@@ -30,25 +35,32 @@ test_exchange_client(void)
 {
 	nng_socket sock;
 	exchange_t *ex;
-	producer_t *p;
 
 	NUTS_TRUE(nng_exchange_client_open(&sock) == 0);
 
-	NUTS_TRUE(exchange_init(&ex, "exchange1") == 0);
+	char **ringBufferName;
+	ringBufferName = nng_alloc(1 * sizeof(char *));
+	for (int i = 0; i < 1; i++) {
+		ringBufferName[i] = nng_alloc(100 * sizeof(char));
+	}
+
+	strcpy(ringBufferName[0], "ringBuffer1");
+
+	int caps = 10000;
+
+	NUTS_TRUE(exchange_init(&ex, "exchange1", "topic1", (void *)&caps, ringBufferName, 1) == 0);
 	NUTS_TRUE(ex != NULL);
 
-
-	NUTS_TRUE(producer_init(&p, match_cb, target_cb) == 0);
-	NUTS_TRUE(p != NULL);
-	NUTS_TRUE(exchange_add_prod(ex, p) == 0);
+	for (int i = 0; i < 1; i++) {
+		nng_free(ringBufferName[i], sizeof(*ringBufferName[i]));
+	}
+	nng_free(ringBufferName, sizeof(*ringBufferName));
 
 	nng_socket_set_ptr(sock, NNG_OPT_EXCHANGE_ADD, ex);
 
-	int msg = 1;
-	nng_sendmsg(sock, (void *)&msg, NNG_FLAG_NONBLOCK);
+	NUTS_TRUE(client_publish(sock, "topic1", NULL, 0, 0, 0) == 0);
+
 	nng_msleep(200);
-	NUTS_TRUE(match_times == 1);
-	NUTS_TRUE(target_times == 1);
 
 	return;
 }
