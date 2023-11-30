8e4d1995167c8c75c7fc21265a2767772fafeaf6
Moi Ran <maoyi.ran@emqx.io>
2023-11-30 14:25:32 +0800

    * ADD [exchange_client] Support find ringbuffer with name in exchange_client
    Signed-off-by: Moi Ran <maoyi.ran@emqx.io>

--- include/nng/exchange/exchange_client.h
+++ include/nng/exchange/exchange_client.h
@@ -2,18 +2,22 @@
 #define EXCHANGE_CLIENT_H
 
 #include "nng/exchange/exchange.h"
-#define nng_exchange_self 0
-#define nng_exchange_self_name "exchange-client"
-#define nng_exchange_peer 0
-#define nng_exchange_peer_name "exchange-server"
-#define nng_opt_exchange_add "exchange-client-add"
+#define nng_exchange_self                0
+#define nng_exchange_self_name           "exchange-client"
+#define nng_exchange_peer                0
+#define nng_exchange_peer_name           "exchange-server"
+#define nng_opt_exchange_add             "exchange-client-add"
 
-#define NNG_EXCHANGE_SELF 0
-#define NNG_EXCHANGE_SELF_NAME "exchange-client"
-#define NNG_EXCHANGE_PEER 0
-#define NNG_EXCHANGE_PEER_NAME "exchange-server"
-#define NNG_OPT_EXCHANGE_ADD "exchange-client-add"
+#define NNG_EXCHANGE_SELF                0
+#define NNG_EXCHANGE_SELF_NAME           "exchange-client"
+#define NNG_EXCHANGE_PEER                0
+#define NNG_EXCHANGE_PEER_NAME           "exchange-server"
+#define NNG_OPT_EXCHANGE_ADD             "exchange-client-add"
+#define NNG_OPT_EXCHANGE_GET_EX_QUEUE    "exchange-client-get-ex-queue"
 
 int nng_exchange_client_open(nng_socket *sock);
+
+int exchange_queue_get_ringBuffer(nni_list *ex_queue,
+								  char *rbName, ringBuffer_t **rb);
 
 #endif
