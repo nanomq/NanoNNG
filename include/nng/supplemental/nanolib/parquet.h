871b6d866b18b87266f98d48512c4ca321f5aa64
lee-emqx <lihj@emqx.io>
2023-12-15 15:48:41 +0800

    * NEW [parquet] rename parquet_conf to conf_parquet.

--- include/nng/supplemental/nanolib/parquet.h
+++ include/nng/supplemental/nanolib/parquet.h
@@ -8,8 +8,6 @@
 extern "C" {
 #endif
 
-typedef conf_parquet parquet_conf;
-
 typedef struct {
 	uint32_t *keys;
 	uint8_t **darray;
@@ -21,7 +19,7 @@ typedef struct {
 parquet_object *parquet_object_alloc(uint32_t *keys, uint8_t **darray,
     uint32_t *dsize, uint32_t size, nng_aio *aio);
 int             parquet_write_batch_async(parquet_object *elem);
-int             parquet_write_launcher(parquet_conf *conf);
+int             parquet_write_launcher(conf_parquet *conf);
 
 const char  *parquet_find(uint32_t key);
 const char **parquet_find_span(uint32_t key, uint32_t offset, uint32_t *size);
