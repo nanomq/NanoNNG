bf81884731a9def04af04012c8333edb1de8feed
lee-emqx <lihj@emqx.io>
2023-12-22 16:59:07 +0800

    * NEW [conf] update conf_parquet_parse_ver2 for encryption.
    Signed-off-by: lee-emqx <lihj@emqx.io>

--- include/nng/supplemental/nanolib/conf.h
+++ include/nng/supplemental/nanolib/conf.h
@@ -325,24 +325,24 @@ typedef enum {
 	AES_GCM_CTR_V1 = 1 
 } cipher_type;
 
-struct conf_parquet_encrypt {
+struct conf_parquet_encryption {
 	bool        enable;
 	const char *key_id;
 	const char *key;
 	cipher_type type;
 };
 
-typedef struct conf_parquet_encrypt conf_parquet_encrypt;
+typedef struct conf_parquet_encryption conf_parquet_encryption;
 
 struct conf_parquet {
-	bool                 enable;
-	char	        	*dir;
-	char	        	*file_name_prefix;
-	uint8_t              file_count;
-	uint8_t              file_index;
-	int32_t              file_size;
-	compression_type     comp_type;
-	conf_parquet_encrypt encryption;
+	bool                    enable;
+	char	           		*dir;
+	char	           		*file_name_prefix;
+	uint8_t                 file_count;
+	uint8_t                 file_index;
+	int32_t                 file_size;
+	compression_type        comp_type;
+	conf_parquet_encryption encryption;
 };
 typedef struct conf_parquet conf_parquet;
 
