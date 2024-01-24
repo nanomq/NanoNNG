99ff2b56b7f6db9890cc99f406b4f093af3a7f63
lee-emqx <lihj@emqx.io>
2024-01-24 16:35:23 +0800

    * MDF [parquet] update header file.
    Signed-off-by: lee-emqx <lihj@emqx.io>

--- include/nng/supplemental/nanolib/parquet.h
+++ include/nng/supplemental/nanolib/parquet.h
@@ -14,21 +14,31 @@ typedef void (*parquet_cb)(parquet_object *arg);
 #define PARQUET_WRITE_SUCCESS 1
 #define PARQUET_WRITE_FAILURE 0
 
+typedef struct {
+	uint32_t start_idx;
+	uint32_t end_idx;
+	char    *filename;
+} parquet_file_range;
+
+typedef enum {
+	HOOK_WRITE,
+	EXCHANGE_WRITE
+} parquet_write_type;
+
 struct parquet_object {
-	uint64_t  *keys;
-	uint8_t  **darray;
-	uint32_t  *dsize;
-	uint32_t   size;
-	nng_aio   *aio;
-	void      *arg;
-	int        result;
-	int        file_size;
-	char     **file_list;
-	parquet_cb cb;
+	uint64_t            *keys;
+	uint8_t            **darray;
+	uint32_t            *dsize;
+	uint32_t             size;
+	nng_aio             *aio;
+	void	        	*arg;
+	parquet_write_type   type;
+	int                  file_size;
+	parquet_file_range **file_ranges;
 };
 
 parquet_object *parquet_object_alloc(uint64_t *keys, uint8_t **darray,
-    uint32_t *dsize, uint32_t size, nng_aio *aio, void *arg);
+    uint32_t *dsize, uint32_t size, nng_aio *aio, void *arg, parquet_write_type type);
 void            parquet_object_free(parquet_object *elem);
 
 void parquet_object_set_cb(parquet_object *obj, parquet_cb cb);
@@ -39,7 +49,6 @@ const char  *parquet_find(uint64_t key);
 const char **parquet_find_span(
     uint64_t start_key, uint64_t end_key, uint32_t *size);
 
-nng_msg *parquet_find_msg(char *filename, uint64_t key);
 
 #ifdef __cplusplus
 }
