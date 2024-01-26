3d5a1e41a67b5c6c6122274ef1502b6facc21de1
Moi Ran <maoyi.ran@emqx.io>
2024-01-26 14:44:20 +0800

    * ADD [ringbuffer] Support get msgs from file by keys
    Signed-off-by: Moi Ran <maoyi.ran@emqx.io>

--- include/nng/supplemental/nanolib/ringbuffer.h
+++ include/nng/supplemental/nanolib/ringbuffer.h
@@ -127,6 +127,8 @@ int ringBuffer_search_msgs_fuzz(ringBuffer_t *rb,
 								nng_msg ***list);
 #ifdef SUPP_PARQUET
 int ringBuffer_get_msgs_from_file(ringBuffer_t *rb, void ***msgs, int **msgLen);
+int ringBuffer_get_msgs_from_file_by_keys(ringBuffer_t *rb, uint64_t *keys, uint32_t count,
+										  void ***msgs, int **msgLen);
 #endif
 
 #endif
