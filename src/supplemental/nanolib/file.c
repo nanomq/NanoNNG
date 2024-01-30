49f78bcc7072be87f5d7e238e8bbcbd95c5e59e2
jaylin <jaylin@emqx.io>
2024-01-30 16:30:48 +0800

    * MDF [nanolib/file] add file/folder creation api
    Signed-off-by: jaylin <jaylin@emqx.io>

--- src/supplemental/nanolib/file.c
+++ src/supplemental/nanolib/file.c
@@ -147,3 +147,170 @@ nano_concat_path(const char *dir, const char *file_name)
 
 	return path;
 }
+
+int
+nano_dir_create(const char *fpath)
+{
+	return nano_mkdir(fpath, 0777);
+}
+
+/**/
+// int file_trunc_to_zero(const char *fpath)
+// {
+// 	int fd;
+
+// 	debug_msg("fpath = %s\n", fpath);
+
+// 	fd = open(fpath, O_WRONLY | O_CREAT | O_TRUNC, MODE);
+// 	if (fd >= 0)
+// 		close(fd);
+
+// 	return 0;
+// }
+
+// /*return 1 if exists*/
+// int file_exists(const char *fpath)
+// {
+// 	struct stat st;
+// 	int ret;
+
+// 	ret = stat(fpath, &st) == 0 ? 1 : 0;
+// 	debug_msg("%s: %i", fpath, ret);
+
+// 	return ret;
+// }
+
+// int file_is_symlink(const char *fpath)
+// {
+// 	int ret;
+// 	struct stat st;
+
+// 	ret = lstat(fpath, &st);
+// 	if (ret != 0)
+// 		return 0;
+
+// 	return S_ISLNK(st.st_mode);
+// }
+
+// int file_size(const char *fpath)
+// {
+// 	int ret;
+// 	struct stat st;
+
+// 	if (!file_exists(fpath))
+// 		return 0;
+
+// 	ret = stat(fpath, &st);
+// 	if (ret != 0)
+// 		return -1;
+
+// 	return st.st_size;
+// }
+
+// int file_create_symlink(const char *file_path, const char *link_path)
+// {
+// 	debug_msg("%s => %s", file_path, link_path);
+// 	return symlink(file_path, link_path);
+// }
+
+// int file_read_int(const char *fpath_fmt, ...)
+// {
+// 	va_list args;
+// 	char buff[10];
+// 	int fd, ret = 0;
+
+// 	va_start(args, fpath_fmt);
+// 	vsnprintf(fpath_tmp, sizeof(fpath_tmp), fpath_fmt, args);
+// 	va_end(args);
+
+// 	if (!file_exists(fpath_tmp))
+// 		goto out;
+
+// 	fd = open(fpath_tmp, O_RDONLY);
+// 	if (fd < 0)
+// 		goto out;
+
+// 	ret = read(fd, buff, sizeof(buff));
+// 	if (ret < 0)
+// 		goto close;
+
+// 	ret = strtol(buff, (char **)NULL, 10);
+// 	debug_msg("fpath = %s, pid = %d", fpath_tmp, ret);
+
+// close:
+// 	close(fd);
+// out:
+// 	return ret;
+// }
+
+// int file_read_string(const char *fpath, char *buff, int buff_len)
+// {
+// 	int fd, ret = 0;
+
+// 	if (!file_exists(fpath))
+// 		goto out;
+
+// 	fd = open(fpath, O_RDONLY);
+// 	if (fd < 0)
+// 		goto out;
+
+// 	ret = read(fd, buff, buff_len);
+// 	if (ret < 0)
+// 		goto close;
+
+// 	if (ret > 0)
+// 		buff[ret - 1] = '\0';
+
+// 	debug_msg("fpath = %s, string = %s", fpath, buff);
+
+// close:
+// 	close(fd);
+// out:
+// 	return ret;
+// }
+
+// int file_delete(const char *fpath)
+// {
+// 	if (!fpath)
+// 		return 0;
+
+// 	debug_msg("%s", fpath);
+// 	return unlink(fpath);
+// }
+/**
+ * Create dir according to the path input
+ * @fpath : /tmp/log/nanomq.log.1
+ * then create dir /tmp/log/, skip nanomq.log.1
+*/
+int file_create_dir(const char *fpath)
+{
+	char *last_slash = NULL, *fpath_edit = NULL;
+	int ret = -1;
+
+	log_info("dir = %s", fpath);
+
+	if (fpath[strlen(fpath) - 1] != '/') {
+		fpath_edit = malloc(strlen(fpath) + 1);
+		if (!fpath_edit)
+			return -1;
+
+		strncpy(fpath_edit, fpath, strlen(fpath) + 1);
+		fpath_edit[strlen(fpath)] = '\0';
+
+		last_slash = strrchr(fpath_edit, '/');
+
+		/* not a single slash in the string ? */
+		if (!last_slash)
+			goto out;
+
+		*last_slash = '\0';
+		fpath = fpath_edit;
+	}
+
+	log_info("mkdir = %s", fpath);
+	ret = mkdir(fpath, 0777);
+out:
+	free(fpath_edit);
+
+	return ret;
+}
