752883849ab704b48b9c1a61d25de4db9203c5a2
lee-emqx <lihj@emqx.io>
2024-02-01 17:21:44 +0800

    * NEW [parquet] udpate header file.
    Signed-off-by: lee-emqx <lihj@emqx.io>

--- include/nng/supplemental/nanolib/parquet.h
+++ include/nng/supplemental/nanolib/parquet.h
@@ -11,6 +11,11 @@ extern "C" {
 typedef struct parquet_object parquet_object;
 typedef void (*parquet_cb)(parquet_object *arg);
 
+typedef enum {
+	WRITE_TO_NORMAL,
+	WRITE_TO_TEMP,
+} parquet_write_type;
+
 typedef struct {
 	uint32_t start_idx;
 	uint32_t end_idx;
@@ -34,8 +39,9 @@ struct parquet_object {
 	uint32_t            *dsize;
 	uint32_t             size;
 	nng_aio             *aio;
-	void	        	*arg;
+	void	        *arg;
 	parquet_file_ranges *ranges;
+	parquet_write_type   type;
 };
 
 parquet_object *parquet_object_alloc(uint64_t *keys, uint8_t **darray,
@@ -47,6 +53,9 @@ void parquet_file_range_free(parquet_file_range *range);
 
 void parquet_object_set_cb(parquet_object *obj, parquet_cb cb);
 int  parquet_write_batch_async(parquet_object *elem);
+// Write a batch to a temporary Parquet file, utilize it in scenarios where a single 
+// file is sufficient for writing, sending, and subsequent deletion.
+int  parquet_write_batch_tmp_async(parquet_object *elem);
 int  parquet_write_launcher(conf_parquet *conf);
 
 const char  *parquet_find(uint64_t key);
