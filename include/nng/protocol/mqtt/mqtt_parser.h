e8927aa60875a65d71bac975e9578071f2871d0d
jaylin <jaylin@emqx.io>
2024-02-11 15:02:28 +0800

    * RM [mqtt_parser] remove duplicated func declaration
    Signed-off-by: jaylin <jaylin@emqx.io>

--- include/nng/protocol/mqtt/mqtt_parser.h
+++ include/nng/protocol/mqtt/mqtt_parser.h
@@ -109,7 +109,6 @@ NNG_DECL property *decode_buf_properties(uint8_t *packet, uint32_t packet_len,
 NNG_DECL property *decode_properties(
     nng_msg *msg, uint32_t *pos, uint32_t *len, bool copy_value);
 NNG_DECL int      encode_properties(nng_msg *msg, property *prop, uint8_t cmd);
-NNG_DECL uint32_t get_properties_len(property *prop);
 NNG_DECL int      property_free(property *prop);
 NNG_DECL property_data *property_get_value(property *prop, uint8_t prop_id);
 NNG_DECL void      property_foreach(property *prop, void (*cb)(property *));
