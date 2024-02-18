26986b906f45939cdc7c1307a49741ec77c97504
lee-emqx <lihj@emqx.io>
2024-02-18 17:38:08 +0800

    * NEW [cmake] add cmake.
    Signed-off-by: lee-emqx <lihj@emqx.io>

--- src/supplemental/nanolib/blf/blf.cc
+++ src/supplemental/nanolib/blf/blf.cc
@@ -7,27 +7,3 @@
 #include <fstream>
 #include <locale>
 
-int test_func()
-{
-    /* open file for writing */
-    Vector::BLF::File file;
-    file.open("test.blf", std::ios_base::out);
-    if (!file.is_open()) {
-        std::cout << "Unable to open file" << std::endl;
-        return -1;
-    }
-
-    /* write a CanMessage */
-    auto * canMessage = new Vector::BLF::CanMessage;
-    canMessage->channel = 1;
-    canMessage->flags = 1; // TX
-    canMessage->dlc = 2;
-    canMessage->id = 0x33;
-    canMessage->data[0] = 0x44;
-    canMessage->data[1] = 0x55;
-    file.write(canMessage);
-
-    /* close file */
-    file.close();
-    return 0;
-}
