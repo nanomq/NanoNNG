a510f51b485c6f1fdd0ffda96b249d972062506e
xinyi-xs <lihj@emqx.io>
2024-02-27 18:42:46 +0800

    * NEW [blf] add blf_object related function.
    Signed-off-by: xinyi-xs <lihj@emqx.io>

--- src/supplemental/nanolib/blf/blf.cc
+++ src/supplemental/nanolib/blf/blf.cc
@@ -57,3 +57,61 @@ create_directory(const std::string &directory_path)
 	    directory_path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 	return (status == 0);
 }
+blf_file_range *
+blf_file_range_alloc(uint32_t start_idx, uint32_t end_idx, char *filename)
+{
+	blf_file_range *range = new blf_file_range;
+	range->start_idx      = start_idx;
+	range->end_idx        = end_idx;
+	range->filename       = nng_strdup(filename);
+	return range;
+}
+
+void
+blf_file_range_free(blf_file_range *range)
+{
+	if (range) {
+		FREE_IF_NOT_NULL(range->filename, strlen(range->filename));
+		delete range;
+	}
+}
+
+blf_object *
+blf_object_alloc(uint64_t *keys, uint8_t **darray, uint32_t *dsize,
+    uint32_t size, nng_aio *aio, void *arg)
+{
+	blf_object *elem    = new blf_object;
+	elem->keys          = keys;
+	elem->darray        = darray;
+	elem->dsize         = dsize;
+	elem->size          = size;
+	elem->aio           = aio;
+	elem->arg           = arg;
+	elem->ranges        = new blf_file_ranges;
+	elem->ranges->range = NULL;
+	elem->ranges->start = 0;
+	elem->ranges->size  = 0;
+	return elem;
+}
+
+void
+blf_object_free(blf_object *elem)
+{
+	if (elem) {
+		FREE_IF_NOT_NULL(elem->keys, elem->size);
+		FREE_IF_NOT_NULL(elem->dsize, elem->size);
+		nng_aio_set_prov_data(elem->aio, elem->arg);
+		nng_aio_set_output(elem->aio, 1, elem->ranges);
+		uint32_t *szp = (uint32_t *) malloc(sizeof(uint32_t));
+		*szp          = elem->size;
+		nng_aio_set_msg(elem->aio, (nng_msg *) szp);
+		DO_IT_IF_NOT_NULL(nng_aio_finish_sync, elem->aio, 0);
+		FREE_IF_NOT_NULL(elem->darray, elem->size);
+		for (int i = 0; i < elem->ranges->size; i++) {
+			blf_file_range_free(elem->ranges->range[i]);
+		}
+		free(elem->ranges->range);
+		delete elem->ranges;
+		delete elem;
+	}
+}
