0b10cacb6a2bbf63fce09d1acef6e9661b489f44
wanghaemq <wangwei@emqx.io>
2024-03-05 03:01:59 -0500

    * FIX [quic] Another way to fix the leakage in cparam. Yes free it in disconnected cb.
    Signed-off-by: wanghaemq <wangwei@emqx.io>

--- src/mqtt/transport/quic/mqtt_quic.c
+++ src/mqtt/transport/quic/mqtt_quic.c
@@ -247,9 +247,6 @@ mqtt_quictran_pipe_fini(void *arg)
 	// nni_lmq_fini(&p->rslmq);
 	nni_mtx_fini(&p->mtx);
 	nni_aio_fini(&p->tmaio);
-#ifdef NNG_HAVE_MQTT_BROKER
-	conn_param_free(p->cparam);
-#endif
 	NNI_FREE_STRUCT(p);
 }
 
