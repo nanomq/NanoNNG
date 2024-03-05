3de511364c6c24149bde58c33b1862c0e13896e0
wanghaemq <wangwei@emqx.io>
2024-03-05 05:28:18 -0500

    * FIX [quic] It's a temporary solution only for emqx broker. When connection just established. This event may be triggered. At this time, quic stream close can't notify transport layer is closed. Because no aio from transport layer is available.
    Signed-off-by: wanghaemq <wangwei@emqx.io>

--- src/supplemental/quic/msquic_dial.c
+++ src/supplemental/quic/msquic_dial.c
@@ -1130,8 +1130,8 @@ msquic_strm_cb(_In_ HQUIC stream, _In_opt_ void *Context,
 		if (!Event->START_COMPLETE.PeerAccepted) {
 			// FIXME Not clear the behavious of the broker.
 			log_warn("Peer refused");
-			quic_stream_cb(QUIC_STREAM_EVENT_SHUTDOWN_COMPLETE, c, 0);
-			break;
+			//quic_stream_cb(QUIC_STREAM_EVENT_SHUTDOWN_COMPLETE, c, 0);
+			//break;
 		}
 
 		quic_stream_cb(QUIC_STREAM_EVENT_START_COMPLETE, c, 0);
