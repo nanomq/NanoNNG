978895e051fd16c69d4347e460966b782127a01d
jaylin <jaylin@emqx.io>
2024-03-26 18:03:30 +0800

    * NEW [mqtt/tcp] support SERVER_KEEPALIVE in CONNACK
    Signed-off-by: jaylin <jaylin@emqx.io>

--- src/mqtt/transport/tcp/mqtt_tcp.c
+++ src/mqtt/transport/tcp/mqtt_tcp.c
@@ -33,9 +33,10 @@ struct mqtt_tcptran_pipe {
 	nni_reap_node    reap;
 	uint32_t         packmax; // MQTT Maximum Packet Size (Max length)
 	uint16_t         peer;    // broker info
-	uint8_t          proto;   // MQTT version
+	uint16_t         keepalive;
 	uint16_t         sndmax;  // MQTT Receive Maximum (QoS 1/2 packet)
 	uint8_t          qosmax;
+	uint8_t          proto;   // MQTT version
 	uint8_t          txlen[sizeof(uint64_t)];
 	uint8_t          rxlen[sizeof(uint64_t)]; // fixed header
 	size_t           rcvmax;
@@ -160,6 +161,8 @@ mqtt_tcptran_pipe_init(void *arg, nni_pipe *npipe)
 	p->packmax == 0 ? p->packmax = (uint32_t)0xFFFFFFFF : p->packmax;
 	p->qosmax  == 0 ? p->qosmax  = 2 : p->qosmax;
 
+	p->keepalive = 0;
+
 	return (0);
 }
 
@@ -250,6 +253,8 @@ mqtt_tcptran_ep_match(mqtt_tcptran_ep *ep)
 #ifdef NNG_HAVE_MQTT_BROKER
 	if (p->cparam == NULL) {
 		p->cparam = nni_get_conn_param_from_msg(ep->connmsg);
+		if (p->keepalive != 0)
+			p->cparam->keepalive_mqtt = p->keepalive;
 		nni_msg_set_conn_param(ep->connmsg, p->cparam);
 	}
 #endif
@@ -382,6 +387,10 @@ mqtt_tcptran_pipe_nego_cb(void *arg)
 			data = property_get_value(ep->property, PUBLISH_MAXIMUM_QOS);
 			if (data) {
 				p->qosmax = data->p_value.u8;
+			}
+			data = property_get_value(ep->property, SERVER_KEEP_ALIVE);
+			if (data) {
+				p->keepalive = data->p_value.u16;
 			}
 		} else {
 			if ((rv = nni_mqtt_msg_decode(p->rxmsg)) != MQTT_SUCCESS) {
