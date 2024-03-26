636de8202a591e57d56555ff4fbb0d17dc3fe081
wayne <yukang.wei@emqx.io>
2024-03-26 17:11:20 +0800

    * MDF [nmq_mqtt] format
    Signed-off-by: wayne <yukang.wei@emqx.io>

--- src/sp/protocol/mqtt/nmq_mqtt.c
+++ src/sp/protocol/mqtt/nmq_mqtt.c
@@ -607,7 +607,6 @@ nano_pipe_init(void *arg, nni_pipe *pipe, void *s)
 	nni_aio_init(&p->aio_timer, nano_pipe_timer_cb, p);
 	nni_aio_init(&p->aio_recv, nano_pipe_recv_cb, p);
 
-
 	p->conn_param  = nni_pipe_get_conn_param(pipe);
 	conn_param_free(p->conn_param);
 	p->id          = nni_pipe_id(pipe);
