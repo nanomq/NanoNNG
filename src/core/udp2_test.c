4b5465d5a76289bf6e748c55b32ab1d588f5043a
wanghaemq <wangwei@emqx.io>
2024-05-13 04:39:19 -0400

    * NEW [core/udp] Add unit test for +test_udp_conn_multicast.
    Signed-off-by: wanghaemq <wangwei@emqx.io>

--- src/core/udp2_test.c
+++ src/core/udp2_test.c
@@ -10,6 +10,7 @@
 #include <nuts.h>
 
 static const char *udp_test_url = "udp://127.0.0.1:8877";
+static const char *udp_test_url_multicast = "udp://239.255.255.250:8877";
 
 void
 test_udp_conn_open(void)
@@ -110,10 +111,45 @@ test_udp_send_recv(void)
 	nng_aio_free(aio);
 }
 
+void
+test_udp_conn_multicast(void)
+{
+	nng_stream_dialer *dialer;
+	nng_aio *          aio;
+	nng_stream *       s;
+	void *             t;
+	uint8_t *          buf;
+	size_t             size = 100;
+
+	// allocate messages
+	NUTS_ASSERT((buf = nng_alloc(size)) != NULL);
+	for (size_t i = 0; i < size; i++) {
+		buf[i] = rand() & 0xff;
+	}
+
+	NUTS_PASS(nng_aio_alloc(&aio, NULL, NULL));
+	nng_aio_set_timeout(aio, 5000); // 5 sec
+
+	NUTS_PASS(nng_stream_dialer_alloc(&dialer, udp_test_url_multicast));
+	nng_stream_dialer_dial(dialer, aio);
+	nng_aio_wait(aio);
+	NUTS_PASS(nng_aio_result(aio));
+
+	NUTS_TRUE((s = nng_aio_get_output(aio, 0)) != NULL);
+	t = nuts_stream_send_start(s, buf, size);
+	NUTS_PASS(nuts_stream_wait(t));
+
+	nng_free(buf, size);
+	nng_stream_free(s);
+	nng_stream_dialer_free(dialer);
+	nng_aio_free(aio);
+}
+
 TEST_LIST = {
 	{ "udp conn open", test_udp_conn_open },
 	{ "udp conn send", test_udp_conn_send },
 	{ "udp send then recv", test_udp_send_recv },
+	//{ "udp conn multicast", test_udp_conn_multicast },
 	{ NULL, NULL },
 };
 
