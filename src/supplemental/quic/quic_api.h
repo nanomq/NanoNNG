caadb31bf63f90dede49dca9e6009266fc8ea7e1
jaylin <jaylin@emqx.io>
2024-05-30 12:22:14 +0800

    * FIX [quic_api] add quic_pipe_fini & prevent reuse of null pipe
    Signed-off-by: jaylin <jaylin@emqx.io>

--- src/supplemental/quic/quic_api.h
+++ src/supplemental/quic/quic_api.h
@@ -46,4 +46,5 @@ extern int quic_aio_send(void *arg, nni_aio *aio);
 // Close a qpipe and free it
 extern int quic_pipe_close(void *qpipe, uint8_t *code);
 
+extern void quic_pipe_fini(void *qpipe);
 #endif
