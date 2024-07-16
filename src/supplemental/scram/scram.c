395d36d9d15d8d68ff321055b3c524a72487f287
wanghaemq <wangwei@emqx.io>
2024-07-16 07:04:48 -0400

    * NEW [supp] Introduce a new module scram.
    Signed-off-by: wanghaemq <wangwei@emqx.io>

---
+++ src/supplemental/scram/scram.c
@@ -0,0 +1,29 @@
+//
+// Copyright 2024 NanoMQ Team, Inc. <wangwei@emqx.io>
+//
+// This software is supplied under the terms of the MIT License, a
+// copy of which should be located in the distribution where this
+// file was obtained (LICENSE.txt).  A copy of the license may also be
+// found online at https://opensource.org/licenses/MIT.
+//
+
+uint8_t *
+scram_client_first_msg(const char *username)
+{
+}
+
+static uint8_t *
+scram_server_first_msg()
+{
+}
+
+int
+scram_handle_client_first_msg()
+{
+}
+
+int
+scram_handle_server_first_msg()
+{
+}
+
---
+++ src/supplemental/scram/scram.h
@@ -0,0 +1,13 @@
+//
+// Copyright 2024 NanoMQ Team, Inc. <wangwei@emqx.io>
+//
+// This software is supplied under the terms of the MIT License, a
+// copy of which should be located in the distribution where this
+// file was obtained (LICENSE.txt).  A copy of the license may also be
+// found online at https://opensource.org/licenses/MIT.
+//
+
+#ifndef NNG_SUPP_SCRAM_H
+#define NNG_SUPP_SCRAM_H
+
+#endif
