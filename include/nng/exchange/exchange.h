d25365185879a6b07a5266b3ea8ffd426b28c9ac
Moi Ran <maoyi.ran@emqx.io>
2024-07-24 19:27:33 +0800

    * ADD [exchange] Support stream type
    Signed-off-by: Moi Ran <maoyi.ran@emqx.io>

--- include/nng/exchange/exchange.h
+++ include/nng/exchange/exchange.h
@@ -2,13 +2,16 @@
 #define EXCHANGE_H
 
 #include <stddef.h>
-#include "core/nng_impl.h"
+#include "nng/nng.h"
 #include "nng/supplemental/nanolib/conf.h"
 
 // Exchange MQ 
 #define EXCHANGE_NAME_LEN 32
 #define TOPIC_NAME_LEN    128
 #define RINGBUFFER_MAX    64
+
+#define STREAM_RAW        0
+#define STREAM_CANMSG     1
 
 typedef struct exchange_s exchange_t;
 struct exchange_s {
@@ -17,14 +20,15 @@ struct exchange_s {
 
 	ringBuffer_t *rbs[RINGBUFFER_MAX];
 	unsigned int rb_count;
+	uint8_t streamType;
 };
 
-NNG_DECL int exchange_client_get_msg_by_key(void *arg, uint64_t key, nni_msg **msg);
+NNG_DECL int exchange_client_get_msg_by_key(void *arg, uint64_t key, nng_msg **msg);
 NNG_DECL int exchange_client_get_msgs_by_key(void *arg, uint64_t key, uint32_t count, nng_msg ***list);
 
 NNG_DECL int exchange_client_get_msgs_fuzz(void *arg, uint64_t start, uint64_t end, uint32_t *count, nng_msg ***list);
 
-NNG_DECL int exchange_init(exchange_t **ex, char *name, char *topic,
+NNG_DECL int exchange_init(exchange_t **ex, char *name, char *topic, uint8_t streamType,
 				  unsigned int *rbsCaps, char **rbsName, uint8_t *rbsFullOp, unsigned int rbsCount);
 NNG_DECL int exchange_add_rb(exchange_t *ex, ringBuffer_t *rb);
 NNG_DECL int exchange_release(exchange_t *ex);
