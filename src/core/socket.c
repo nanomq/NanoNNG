256b7f0e357ebfa654abbfbaeaa71063b94fbba2
Garrett D'Amore <garrett@damore.org>
2024-11-02 23:54:27 -0700

    Replace `NNG_OPT_RAW` with `nng_socket_raw`.
    Similar to the other identities, this simplifies code a bit.
    
    Signed-off-by: jaylin <jaylin@emqx.io>

--- src/core/socket.c
+++ src/core/socket.c
@@ -136,13 +136,6 @@ sock_get_fd(nni_sock *s, unsigned flag, int *fdp)
 }
 
 static int
-sock_get_raw(void *s, void *buf, size_t *szp, nni_type t)
-{
-	bool raw = ((nni_sock_flags(SOCK(s)) & NNI_PROTO_FLAG_RAW) != 0);
-	return (nni_copyout_bool(raw, buf, szp, t));
-}
-
-static int
 sock_set_recvtimeo(void *s, const void *buf, size_t sz, nni_type t)
 {
 	return (nni_copyin_ms(&SOCK(s)->s_rcvtimeo, buf, sz, t));
@@ -251,10 +244,6 @@ static const nni_option sock_options[] = {
 	    .o_name = NNG_OPT_SOCKNAME,
 	    .o_get  = sock_get_sockname,
 	    .o_set  = sock_set_sockname,
-	},
-	{
-	    .o_name = NNG_OPT_RAW,
-	    .o_get  = sock_get_raw,
 	},
 	// terminate list
 	{
