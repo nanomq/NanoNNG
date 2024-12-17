83f8853e26e281bb9370db156083e20d58475ed3
xinyi-xs <lihj@emqx.io>
2024-12-17 16:54:11 +0800

    * NEW [parquet_file_manager] add parquet_file_manager.fetch_queue function.
    Signed-off-by: xinyi-xs <lihj@emqx.io>

--- src/supplemental/nanolib/parquet/parquet_file_manager.cc
+++ src/supplemental/nanolib/parquet/parquet_file_manager.cc
@@ -7,3 +7,20 @@ parquet_file_manager::add_queue(conf_exchange_node *node)
 	file_queue_map[node->name] = make_shared<parquet_file_queue>(node->parquet);
 	file_queue_map[node->name]->init();
 }
+
+void
+parquet_file_manager::remove_queue()
+{
+}
+
+CircularQueue *
+parquet_file_manager::fetch_queue(const string &topic)
+{
+
+    auto it = file_queue_map.find(topic);
+    if (it != file_queue_map.end()) {
+        return it->second->get_queue();
+    } else {
+        return nullptr;
+    }
+}
