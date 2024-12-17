3d4f4260f11b10e3e870410d3126707d65922ac0
xinyi-xs <lihj@emqx.io>
2024-12-17 16:52:13 +0800

    * NEW [parquet_file_queue] add parquet_file_manager class.
    Signed-off-by: xinyi-xs <lihj@emqx.io>

---
+++ src/supplemental/nanolib/parquet/parquet_file_manager.h
@@ -0,0 +1,21 @@
+#ifndef PARQUET_FILE_MANAGER_H
+#define PARQUET_FILE_MANAGER_H
+
+#include "parquet_file_queue.h"
+
+#include <unordered_map>
+
+class parquet_file_manager {
+    public:
+	void update_queue(const string &topic, const char *filename);
+	void add_queue(conf_exchange_node *node);
+    CircularQueue* fetch_queue(const string &topic);
+	conf_parquet* fetch_conf(const string &topic);
+	void remove_queue();
+
+    private:
+	unordered_map<string, shared_ptr<parquet_file_queue>>
+	    file_queue_map;
+};
+
+#endif // PARQUET_FILE_MANAGER_H
