89cd5b517441f300116419201eacc47ef745272a
xinyi-xs <lihj@emqx.io>
2024-12-17 16:44:18 +0800

    * NEW [parquet_file_queue] add parquet_file_queue destructor.
    Signed-off-by: xinyi-xs <lihj@emqx.io>

--- src/supplemental/nanolib/parquet/parquet_file_queue.cc
+++ src/supplemental/nanolib/parquet/parquet_file_queue.cc
@@ -1,9 +1,18 @@
 #include"parquet_file_queue.h"
-#include <unistd.h>
 
 // Constructor
 parquet_file_queue::parquet_file_queue(conf_parquet *node)
     : node(node)
 {
 	INIT_QUEUE(queue);
+}
+
+// Destructor
+parquet_file_queue::~parquet_file_queue()
+{
+	while (!IS_EMPTY(queue)) {
+		char *name = (char *) DEQUEUE(queue);
+		free(name);
+	}
+	DESTROY_QUEUE(queue);
 }
