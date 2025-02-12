72fecdffdbeccb6bd1ae1056c8004dc512888572
wangha <wanghamax@gmail.com>
2025-02-12 05:14:07 -0500

    * NEW [ssl] Add more logs.
    Signed-off-by: wangha <wanghamax@gmail.com>

--- src/mqtt/transport/tls/mqtt_tls.c
+++ src/mqtt/transport/tls/mqtt_tls.c
@@ -1594,7 +1594,7 @@ mqtts_tcptran_ep_connect(void *arg, nni_aio *aio)
 		ep->backoff = ep->backoff > ep->backoff_max
 		    ? (nni_duration) (nni_random() % 1000)
 		    : ep->backoff;
-		log_warn("reconnect in %ld", ep->backoff);
+		log_warn("reconnect %s in %ld", ep->url->u_host, ep->backoff);
 		nni_msleep(ep->backoff);
 	} else {
 		ep->backoff = nni_random()%2000;
