d200d45c4b8056fb82bb3fd537bd75db913e599d
wangha <wanghamax@gmail.com>
2025-02-11 22:29:48 -0500

    * NEW [ssl] Remove print_trace.
    Signed-off-by: wangha <wanghamax@gmail.com>

--- src/supplemental/tls/openssl/openssl.c
+++ src/supplemental/tls/openssl/openssl.c
@@ -40,23 +40,6 @@
 #endif
 
 #ifdef OPEN_DEBUG
-#include <execinfo.h>
-static void
-print_trace()
-{
-	void  *array[10];
-	char **strings;
-	int    size, i;
-
-	size    = backtrace(array, 10);
-	strings = backtrace_symbols(array, size);
-	if (strings != NULL) {
-		printf("Obtained %d stack frames.\n", size);
-		for (i = 0; i < size; i++)
-			printf("%s\n", strings[i]);
-	}
-	free(strings);
-}
 
 static void
 print_hex(char *str, const uint8_t *data, size_t len)
@@ -69,11 +52,6 @@ print_hex(char *str, const uint8_t *data, size_t len)
 }
 
 #else
-
-static void
-print_trace()
-{
-}
 
 static void
 print_hex(char *str, const uint8_t *data, size_t len)
@@ -302,7 +280,6 @@ static int
 open_conn_handshake(nng_tls_engine_conn *ec)
 {
 	int rv;
-	//print_trace();
 	if (ec->ok == 1)
 		return 0;
 	rv = SSL_do_handshake(ec->ssl);
