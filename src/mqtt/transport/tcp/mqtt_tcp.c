e8933c428b0383adc19eba004746d7ad274b4b02
jaylin <jaylin@emqx.io>
2025-03-17 19:31:27 +0800

    * MDF [mqtt/transport] sync changes from oss ver
    Signed-off-by: jaylin <jaylin@emqx.io>

--- src/mqtt/transport/tcp/mqtt_tcp.c
+++ src/mqtt/transport/tcp/mqtt_tcp.c
@@ -774,6 +774,7 @@ mqtt_tcptran_pipe_recv_cb(void *arg)
 			} else if (qos_pac == 2) {
 				ack_cmd = CMD_PUBREC;
 			} else {
+				log_warn("Wrong QoS level!");
 				rv = PROTOCOL_ERROR;
 				goto recv_error;
 			}
@@ -1595,7 +1596,7 @@ mqtt_tcptran_ep_connect(void *arg, nni_aio *aio)
 		ep->backoff = ep->backoff > ep->backoff_max
 		    ? (nni_duration) (nni_random() % 1000)
 		    : ep->backoff;
-		log_warn("reconnect in %ld ms", ep->backoff);
+		log_warn("reconnect to %s in %ld ms", ep->url->u_host, ep->backoff);
 		nni_msleep(ep->backoff);
 	} else {
 		ep->backoff = nni_random()%2000;
