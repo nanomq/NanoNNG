0c58a371dc90feee7ac2f8f9bb89b6a27b1e4002
jaylin <jaylin@emqx.io>
2025-04-14 14:28:53 +0800

    * NEW [conf] add cancel_timeout for http webhook
    Signed-off-by: jaylin <jaylin@emqx.io>

--- src/supplemental/nanolib/conf_ver2.c
+++ src/supplemental/nanolib/conf_ver2.c
@@ -674,7 +674,8 @@ conf_webhook_parse_ver2(conf *config, cJSON *jso)
 		    jso_webhook, webhook_encoding);
 		hocon_read_num_base(
 		    webhook, pool_size, "pool_size", jso_webhook);
-
+		hocon_read_num_base(
+		    webhook, cancel_time, "cancel_time", jso_webhook);
 		cJSON    *jso_webhook_tls = hocon_get_obj("ssl", jso_webhook);
 		conf_tls *webhook_tls     = &(webhook->tls);
 		conf_tls_parse_ver2_base(webhook_tls, jso_webhook_tls);
