5a97b06cf685858d61dce7ca653246bc669d75c3
wangha <wanghamax@gmail.com>
2025-05-14 06:05:06 -0400

    * NEW [retains] Introduce new file retains.c retains.h.
    Signed-off-by: wangha <wanghamax@gmail.com>

---
+++ include/nng/supplemental/nanolib/retains.h
@@ -0,0 +1,20 @@
+#ifndef NANOLIBS_RETAINS_H
+#define NANOLIBS_RETAINS_H
+
+#include "nng/nng.h"
+#include "nng/supplemental/util/idhash.h"
+
+struct retains_db_item {
+	char    *topic;
+	uint8_t  qos;
+	char    *clientid;
+	nng_time ts;
+	nng_msg *msg;
+};
+typedef struct retains_db_item retains_db_item;
+
+int    retains_db_add_item(nng_id_map *map, const char *topic, const char *clientid, nng_msg *msg);
+void   retains_db_rm_item(nng_id_map *map, const char *topic);
+char * retains_json_all_items(nng_id_map *map);
+
+#endif
---
+++ src/supplemental/nanolib/retains.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "nng/protocol/mqtt/mqtt_parser.h"
+#include "nng/supplemental/nanolib/retains.h"
+#include "nng/supplemental/nanolib/cJSON.h"
+#include "nng/supplemental/nanolib/conf.h"
+#include "nng/supplemental/util/idhash.h"
+#include "nng/nng.h"
+#include "core/nng_impl.h"
+#include "core/sockimpl.h"
+
+static retains_db_item *
+new_item(const char *topic, const char *clientid, nng_msg *msg)
+{
+	retains_db_item *item = nng_alloc(sizeof(retains_db_item));
+	item->topic = strdup(topic);
+	item->clientid = strdup(clientid);
+	nng_msg_clone(msg);
+	item->msg = msg;
+	return item;
+}
+
+static void
+free_item(retains_db_item *item)
+{
+	nng_free(item->clientid, 0);
+	nng_free(item->topic, 0);
+	nng_msg_free(item->msg);
+	nng_free(item, 0);
+}
+
+int
+retains_db_add_item(nng_id_map *map, const char *topic, const char *clientid, nng_msg *msg)
+{
+	uint32_t id = DJBHashn((char *)topic, strlen(topic));
+	retains_db_item *old;
+	retains_db_item *item = new_item(topic, clientid, msg);
+	if (NULL != (old = nng_id_get(map, id))) {
+		free_item(old);
+	}
+	return nng_id_set(map, id, item);
+}
+
+void
+retains_db_rm_item(nng_id_map *map, const char *topic)
+{
+	uint32_t id = DJBHashn((char *)topic, strlen(topic));
+	retains_db_item *old;
+	if (NULL != (old = nng_id_get(map, id))) {
+		free_item(old);
+	}
+	nng_id_remove(map, id);
+}
+
+static inline void
+iter_retains_db(void *k, void *v, void *arg)
+{
+	if (!v)
+		return;
+	(void) k;
+
+	retains_db_item *item = v;
+	cJSON *retainjson = cJSON_CreateObject();
+	cJSON_AddStringToObject(retainjson, "topic", item->topic);
+	cJSON_AddStringToObject(retainjson, "clientid", item->clientid);
+
+	cJSON_AddItemToArray(retainjson, arg);
+}
+
+char *
+retains_json_all_items(nng_id_map *map)
+{
+	cJSON *resjson = cJSON_CreateObject();
+	cJSON *arrjson = cJSON_CreateArray();
+	nng_id_map_foreach2(map, iter_retains_db, arrjson);
+	cJSON_AddItemToObject(resjson, "retains", arrjson);
+	return cJSON_PrintUnformatted(resjson);
+}
+
