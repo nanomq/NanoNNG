5f340855e6a979497dab066c9c45c34c8a032681
xinyi-xs <lihj@emqx.io>
2025-05-19 18:23:36 +0800

    * NEW [topics] add calculate_topic_hierarchy function.
    Signed-off-by: xinyi-xs <lihj@emqx.io>

--- src/supplemental/nanolib/topics.c
+++ src/supplemental/nanolib/topics.c
@@ -12,6 +12,35 @@ static int count_slashes(const char* str, size_t len) {
 
 
 
+static void calculate_topic_hierarchy(topics* s) {
+
+    char* current = s->local_topic;
+    while ((current = strpbrk(current, "+#")) != NULL) {
+        s->skip_level = s->save_level = -2;
+        if (*current == '#') {
+            if (current != s->local_topic + s->local_topic_len - 1) {
+                s->skip_level = s->save_level = -3;
+                // log_error
+                return;
+            }
+        }
+        current++;
+    }
+
+    char* substr_pos = strstr(s->remote_topic, s->local_topic);
+    if (!substr_pos) return;
+
+    size_t prefix_length = substr_pos - s->remote_topic;
+    s->skip_level = count_slashes(s->remote_topic, prefix_length);
+
+    size_t remaining_len = strlen(substr_pos);
+    if (remaining_len == s->local_topic_len) {
+        s->save_level = -1;
+    } else if (remaining_len > s->local_topic_len) {
+        s->save_level = 1 + count_slashes(s->local_topic, s->local_topic_len);
+    }
+}
+
 
 
 
