c5cd9c57a0f9542703a4b13fd7f8c7293fb998b7
xinyi-xs <lihj@emqx.io>
2025-05-19 18:31:12 +0800

    * NEW [topics] add generate_optimized_topic function.
    Signed-off-by: xinyi-xs <lihj@emqx.io>

--- src/supplemental/nanolib/topics.c
+++ src/supplemental/nanolib/topics.c
@@ -10,8 +10,8 @@ static int count_slashes(const char* str, size_t len) {
     return count;
 }
 
-
-static void calculate_topic_hierarchy(topics* s) {
+static void calculate_topic_hierarchy(topics* s)
+{
 
     char* current = s->local_topic;
     while ((current = strpbrk(current, "+#")) != NULL) {
@@ -40,7 +40,8 @@ static void calculate_topic_hierarchy(topics* s) {
     }
 }
 
-void validate_and_preprocess_topics(topics* s) {
+void validate_and_preprocess_topics(topics* s)
+{
     if (!s || !s->remote_topic || !s->local_topic) {
         fprintf(stderr, "Invalid input parameters\n");
         return;
@@ -53,4 +54,29 @@ void validate_and_preprocess_topics(topics* s) {
 
 }
 
+void generate_optimized_topic(const topics* s, const char *topic)
+{
+    char* current_pos = topic;
+    for (int i = 0; i < s->skip_level && current_pos; ++i) {
+        current_pos = strchr(current_pos, '/');
+        if (current_pos) ++current_pos;
+    }
+
+    char* new_topic = NULL;
+    if (s->save_level == -1) {
+        new_topic = strdup(current_pos ? current_pos : "");
+    } else if (s->save_level == -2) {
+        new_topic = strdup(s->local_topic);
+    } else {
+        char* end_pos = current_pos;
+        for (int i = 0; end_pos && i < s->save_level; ++i) {
+            end_pos = strchr(end_pos, '/');
+            if (end_pos) ++end_pos;
+        }
+        new_topic = end_pos ? strndup(current_pos, end_pos - current_pos - 1) : strdup(current_pos);
+    }
+
+    printf("Optimized topic: %s\n", new_topic);
+    free(new_topic);
+}
 
