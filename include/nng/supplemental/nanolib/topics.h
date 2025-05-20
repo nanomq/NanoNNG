7f9f665e3a2a80df0e4cba9fedd034c37ba4175f
xinyi-xs <lihj@emqx.io>
2025-05-20 15:54:32 +0800

    * NEW [topics] update header file.
    Signed-off-by: xinyi-xs <lihj@emqx.io>

--- include/nng/supplemental/nanolib/topics.h
+++ include/nng/supplemental/nanolib/topics.h
@@ -2,11 +2,14 @@
 #define MQTT_TOPICS_H
 
 #include "nng/nng.h"
-#include "nng/supplemental/util/platform.h"
 #include "nng/supplemental/nanolib/conf.h"
+#include "nng/supplemental/util/platform.h"
 
+#define LOCAL_TOPIC_INVALID_LEVEL (-3)
+#define LOCAL_TOPIC_DEFAULT_LEVEL (-2)
+#define LOCAL_TOPIC_INFINITE_LEVEL (-1)
 
-NNG_DECL void preprocess_topics(topics* s);
-NNG_DECL void generate_repub_topic(const topics* s, const char* topic);
+NNG_DECL void  preprocess_topics(topics *s);
+NNG_DECL char *generate_repub_topic(const topics *s, char *topic);
 
 #endif
