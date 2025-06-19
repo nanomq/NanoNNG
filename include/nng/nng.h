e9d2da03403d887184904ec47168e250d710cd9f
jaylin <jaylin@emqx.io>
2025-06-19 17:21:24 +0800

    * MDF [nng] new nng_hocon_parse_str wrapper
    Signed-off-by: jaylin <jaylin@emqx.io>

--- include/nng/nng.h
+++ include/nng/nng.h
@@ -1367,6 +1367,7 @@ typedef struct conn_param        conn_param;
 typedef struct pub_packet_struct pub_packet_struct;
 typedef struct pipe_db           nano_pipe_db;
 
+NNG_DECL void *nng_hocon_parse_str(char *str, size_t len);
 NNG_DECL int nng_access(const char* name, int flag);
 
 // NANOMQ MQTT API ends
