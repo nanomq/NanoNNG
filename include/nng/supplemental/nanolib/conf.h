20b28540b1598de6144bada9e0b215af5e234b57
Elia-Renzoni <elia.renzoni03@gmail.com>
2025-06-25 11:02:34 +0200

    NEW [raft] add raft conf struct and init functions
    Signed-off-by: Elia-Renzoni <elia.renzoni03@gmail.com>

--- include/nng/supplemental/nanolib/conf.h
+++ include/nng/supplemental/nanolib/conf.h
@@ -446,6 +446,36 @@ struct conf_preset_session {
 };
 
 typedef struct conf_preset_session conf_preset_session;
+
+struct conf_raft {
+	size_t raft_group_count;
+	uint16_t majority_quorum_count;
+	conf_raft_node **raft_group;
+	bool leader_status;
+};
+
+typedef struct conf_raft conf_raft;
+
+struct conf_raft_node {
+	char *address;
+	char *host;
+	uint16_t port;
+	uint16_t leader_election_timeout;
+	conf_tcp raft_tcp_conf;
+	conf_raft_conn raft_packets;
+	nng_dialer *raft_node_dialer;
+	nng_aio **raft_node_aio;
+};
+
+typedef struct conf_raft_node conf_raft_node;
+
+struct conf_raft_conn {
+	uint32_t max_replication_packet_size;
+	uint16_t max_election_packet_size;
+};
+
+typedef struct conf_raft_conn conf_raft_conn;
+
 typedef struct {
 	char *zmq_sub_url;
 	char *zmq_pub_url;
@@ -617,6 +647,7 @@ struct conf {
 	bool       daemon;
 	bool       ipc_internal;
 	bool       bridge_mode;				// global switch of bridging for hot update
+	bool 	   raft_mode;
 
 	conf_tcp_list        tcp_list;
 	conf_tls_list        tls_list;
@@ -627,6 +658,7 @@ struct conf {
 	conf_preset_session  pre_sessions;
 	conf_bridge          bridge;		//standard MQTT
 	conf_bridge          aws_bridge;	// AWS IoT Core
+	conf_raft            raft;
 	conf_exchange        exchange;
 	conf_parquet         parquet;
 	conf_blf             blf;
