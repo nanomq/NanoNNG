5075a8678a77db48d8872f5d2e6cf0841e4a25ac
jaylin <jaylin@emqx.io>
2025-06-26 20:58:22 +0800

    * MDF [conf] restore enable option for auths
    Signed-off-by: jaylin <jaylin@emqx.io>

--- src/supplemental/nanolib/conf.c
+++ src/supplemental/nanolib/conf.c
@@ -473,6 +473,12 @@ conf_basic_parse(conf *config, const char *path)
 			    nni_strcasecmp(value, "yes") == 0 ||
 			    nni_strcasecmp(value, "true") == 0;
 			nng_strfree(value);
+		} else if ((value = get_conf_value(
+		                line, sz, "auth_enable")) != NULL) {
+			config->auths.enable =
+			    nni_strcasecmp(value, "yes") == 0 ||
+			    nni_strcasecmp(value, "true") == 0;
+			nng_strfree(value);
 		} else if ((value = get_conf_value(line, sz, "acl_nomatch")) !=
 		    NULL) {
 			config->acl_nomatch =
