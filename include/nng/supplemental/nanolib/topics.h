32955bd70fa3ed236d21f2740da4d85a5be1b493
jaylin <jaylin@emqx.io>
2025-08-12 19:06:27 +0800

    * MDF [nanolib/topics] merge sub/forward topic mapping func
    Signed-off-by: jaylin <jaylin@emqx.io>

--- include/nng/supplemental/nanolib/topics.h
+++ include/nng/supplemental/nanolib/topics.h
@@ -9,11 +9,8 @@
 #define LOCAL_TOPIC_DEFAULT_LEVEL (-2)
 #define LOCAL_TOPIC_INFINITE_LEVEL (-1)
 
-NNG_DECL void  preprocess_topics(topics *s);
+NNG_DECL void  preprocess_topics(topics *s, bool is_sub);
 
-NNG_DECL void  preprocess_topics2(topics *s);
-
-NNG_DECL char *generate_repub_topic(const topics *s, char *topic);
-NNG_DECL char *generate_repub_topic2(const topics *s, char *topic);
+NNG_DECL char *generate_repub_topic(const topics *s, char *topic, bool is_sub);
 
 #endif
