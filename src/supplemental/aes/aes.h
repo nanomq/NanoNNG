f5da168a70304a9488e5625104254663e304fdf8
wangha <wanghamax@gmail.com>
2025-08-22 03:12:27 -0400

    * NEW [aes] Update aes enc/dec interfaces.
    Signed-off-by: wangha <wanghamax@gmail.com>

--- src/supplemental/aes/aes.h
+++ src/supplemental/aes/aes.h
@@ -2,9 +2,7 @@
 #define NNG_SUPPLEMENTAL_AES_AES_H
 
 // Refer to nanomq/extern/aes_gcm.c.
-char* nni_aes_gcm_decrypt(char *ciphertext, int ciphertext_len,
-		char *key, char *tag, int *plaintext_lenp);
-char* nni_aes_gcm_encrypt(char *plain, int plainsz, char *key,
-		char **tagp, int *cipher_lenp);
+char* nni_aes_gcm_decrypt(char *cipher, int cipher_len, char *key, int *plain_lenp);
+char* nni_aes_gcm_encrypt(char *plain, int plain_len, char *key, int *cipher_lenp);
 
 #endif
