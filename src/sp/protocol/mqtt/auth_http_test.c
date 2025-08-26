b75db256f4b6f799878202b0c019958192619b28
jaylin <jaylin@emqx.io>
2025-08-26 18:40:01 +0800

    * MDF [auth_http_test] bypass strange github CI fail
    Signed-off-by: jaylin <jaylin@emqx.io>

--- src/sp/protocol/mqtt/auth_http_test.c
+++ src/sp/protocol/mqtt/auth_http_test.c
@@ -60,12 +60,14 @@ void test_auth_http_sub_pub(void)
 	conf_auth_http *conf = NULL;
 	conf_auth_http_init(&conf);
 	NUTS_TRUE(conf != NULL);
-	char *url = "http://127.0.0.1:8064/mqtt/acl";
+	char *url = "http://10.1.0.1:8964/mqtt/acl";
 	conf->super_req.enable = false;
 	conf->acl_req.url = nng_alloc(strlen(url) + 1);
 	strncpy(conf->acl_req.url, url, strlen(url));
 	conf->acl_req.url[strlen(url)] = '\0';
 	conf->super_req.url = NULL;
+	conf->connect_timeout = 1;
+	conf->timeout = 1;
 
 	conn_param *conn_param = NULL;
 	conn_param_init(&conn_param);
@@ -80,13 +82,15 @@ void test_auth_http_sub_pub(void)
 	/* handle pub */
 	int rc = nmq_auth_http_sub_pub(conn_param, false, tq, conf);
 	/* send_request will be failed */
-	NUTS_TRUE(rc != 0);
+	printf("rc %d\n", rc);
+	NUTS_TRUE(rc == NOT_AUTHORIZED);
 
 	topic_queue *tq2 = topic_queue_init("topic2", strlen("topic2"));
 	tq->next = tq2;
 	rc = nmq_auth_http_sub_pub(conn_param, true, tq, conf);
 	/* send_request will be failed */
-	NUTS_TRUE(rc != 0);
+	printf("rc %d\n", rc);
+	NUTS_TRUE(rc == NOT_AUTHORIZED);
 
 	topic_queue_release(tq);
 	nng_mtx_free(conf->acl_req.mtx);
