4f6a8160f5dde301d24925dfaa18d6b2e9cf9dea
xinyi-xs <lihj@emqx.io>
2025-09-29 17:06:24 +0800

    * NEW [parquet] add get_queue_index function && make all function inline.
    Signed-off-by: xinyi-xs <lihj@emqx.io>

--- src/supplemental/nanolib/parquet/parquet_file_manager.h
+++ src/supplemental/nanolib/parquet/parquet_file_manager.h
@@ -5,18 +5,66 @@
 
 #include <unordered_map>
 
+// Make it default inline
 class parquet_file_manager {
     public:
-	void update_queue(const string &topic, const char *filename);
-	void add_queue(conf_exchange_node *node);
-    CircularQueue* fetch_queue(const string &topic);
-	conf_parquet* fetch_conf(const string &topic);
-	uint64_t get_queue_sum(const string &topic);
-	void remove_queue();
+	void update_queue(const string &topic, const char *filename)
+	{
+		auto it = file_queue_map.find(topic);
+		if (it != file_queue_map.end()) {
+			it->second->update_queue(filename);
+		}
+	}
+	void add_queue(conf_exchange_node *node)
+	{
+		file_queue_map[node->topic] =
+		    make_shared<parquet_file_queue>(node->parquet);
+		file_queue_map[node->topic]->init();
+	}
+	CircularQueue *fetch_queue(const string &topic) const
+	{
+		auto           it    = file_queue_map.find(topic);
+		CircularQueue *queue = nullptr;
+		if (it != file_queue_map.end()) {
+			queue = it->second->get_queue();
+		}
+		return queue;
+	}
+
+	conf_parquet *fetch_conf(const string &topic) const
+	{
+		auto          it   = file_queue_map.find(topic);
+		conf_parquet *conf = nullptr;
+		if (it != file_queue_map.end()) {
+			conf = it->second->get_conf();
+		}
+		return conf;
+		;
+	}
+
+	uint64_t get_queue_sum(const string &topic) const
+	{
+		auto     it  = file_queue_map.find(topic);
+		uint64_t sum = 0;
+		if (it != file_queue_map.end()) {
+			sum = it->second->get_sum();
+		}
+		return sum;
+	}
+
+	uint32_t get_queue_index(const string &topic) const
+	{
+		auto     it    = file_queue_map.find(topic);
+		uint32_t index = 0;
+		if (it != file_queue_map.end()) {
+			index = it->second->get_index();
+		}
+		return index;
+	}
+	void remove_queue() { };
 
     private:
-	unordered_map<string, shared_ptr<parquet_file_queue>>
-	    file_queue_map;
+	unordered_map<string, shared_ptr<parquet_file_queue>> file_queue_map;
 };
 
 #endif // PARQUET_FILE_MANAGER_H
