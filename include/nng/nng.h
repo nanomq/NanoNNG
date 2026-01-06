ebc0af7a0e688a49321e547b73b9015de875488b
Moi Ran <maoyi.ran@emqx.io>
2026-01-06 10:25:13 +0800

    * NEW [tls_common] Support tls_get_peer_subject
    Signed-off-by: Moi Ran <maoyi.ran@emqx.io>

--- include/nng/nng.h
+++ include/nng/nng.h
@@ -851,6 +851,12 @@ NNG_DECL nng_listener nng_pipe_listener(nng_pipe);
 // `NNG_TLS_AUTH_MODE_NONE`.
 #define NNG_OPT_TLS_PEER_ALT_NAMES "tls-peer-alt-names"
 
+// NNG_OPT_TLS_PEER_SUBJECT returns the full subject (DN) string of
+// the peer certificate. Typically, this is read-only and only
+// available for pipes. This option may return incorrect results if
+// peer authentication is disabled with `NNG_TLS_AUTH_MODE_NONE`.
+#define NNG_OPT_TLS_PEER_SUBJECT "tls-peer-subject"
+
 // TCP options.  These may be supported on various transports that use
 // TCP underneath such as TLS, or not.
 
