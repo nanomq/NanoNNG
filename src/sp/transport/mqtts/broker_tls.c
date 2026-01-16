e11904825f0a519717ba7f41f4817dded066936e
Jaylin <jaylin@emqx.io>
2026-01-16 20:44:24 +0800

    * MDF [mqtt/trasnport] init subinfol in MQTT transport layer
    Signed-off-by: Jaylin <jaylin@emqx.io>

--- src/sp/transport/mqtts/broker_tls.c
+++ src/sp/transport/mqtts/broker_tls.c
@@ -208,6 +208,8 @@ tlstran_pipe_init(void *arg, nni_pipe *npipe)
 	nni_lmq_init(&p->rslmq, 16);
 	nni_atomic_init_bool(&p->closed);
 	p->qos_buf = nng_zalloc(16 + NNI_NANO_MAX_PACKET_SIZE);
+	p->npipe->subinfol = nni_zalloc(sizeof(nni_list));
+	NNI_LIST_INIT(p->npipe->subinfol, struct subinfo, node);
 	log_trace(" ************ tlstran_pipe_init [%p] ************ ", p);
 	return (0);
 }
