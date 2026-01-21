ccd63ed248a298299dbb83be032d8972f05d8b0a
Jaylin <jaylin@emqx.io>
2026-01-21 15:42:04 +0800

    * MDF [conf] Release ACL cache related resources to avoid memory leaks
    Signed-off-by: Moi Ran <maoyi.ran@emqx.io>
    Signed-off-by: Jaylin <jaylin@emqx.io>

--- src/supplemental/nanolib/conf.c
+++ src/supplemental/nanolib/conf.c
@@ -4399,7 +4399,10 @@ conf_auth_http_destroy(conf_auth_http *auth_http)
 	conf_auth_http_req_destroy(&auth_http->auth_req);
 	conf_auth_http_req_destroy(&auth_http->super_req);
 	conf_auth_http_req_destroy(&auth_http->acl_req);
-	nng_mtx_free(auth_http->acl_cache_mtx);
+	if (auth_http->acl_cache_mtx != NULL) {
+		nng_mtx_free(auth_http->acl_cache_mtx);
+		auth_http->acl_cache_mtx = NULL;
+	}
 }
 
 static void
