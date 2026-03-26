b225e95f7913500783031a0eabc3485c585a626f
Josua <josua.palmstedt@tricera.energy>
2026-03-26 17:41:38 +0100

    Add try_private option to control connection bridge bit (0x80)
    Signed-off-by: Josua <josua.palmstedt@tricera.energy>

--- src/supplemental/nanolib/conf.c
+++ src/supplemental/nanolib/conf.c
@@ -3120,6 +3120,7 @@ conf_bridge_node_init(conf_bridge_node *node)
 	node->username       = NULL;
 	node->password       = NULL;
 	node->proto_ver      = 4;
+	node->try_private    = true;
 	node->keepalive      = 60;
 	node->backoff_max    = 5;
 	node->forwards_count = 0;
@@ -3266,6 +3267,10 @@ conf_bridge_node_parse_with_name(const char *path,const char *key_prefix, const 
 		} else if ((value = get_conf_value_with_prefix2(line, sz,
 		                key_prefix, name, ".backoff_max")) != NULL) {
 			node->backoff_max = atoi(value);
+			free(value);
+		} else if ((value = get_conf_value_with_prefix2(line, sz,
+		                key_prefix, name, ".try_private")) != NULL) {
+			node->try_private = nni_strcasecmp(value, "true") == 0;
 			free(value);
 #if defined(SUPP_QUIC)
 		} else if ((value = get_conf_value_with_prefix2(line, sz,
@@ -3692,6 +3697,8 @@ print_bridge_conf(conf_bridge *bridge, const char *prefix)
 		    node->name, node->username);
 		log_info("%sbridge.mqtt.%s.keepalive:                  %d", prefix,
 		    node->name, node->keepalive);
+		log_info("%sbridge.mqtt.%s.try_private:                %s", prefix,
+		    node->name, node->try_private ? "true" : "false");
 		log_info("%sbridge.mqtt.%s.backoff_max:                %d", prefix,
 		    node->name, node->backoff_max);
 		log_info("%sbridge.mqtt.%s.max_parallel_processes:     %ld", prefix,
