f345210ac1436a343545e93b64faa07b99fb0a89
JaylinYu <letrangerjaylin@gmail.com>
2026-07-15 16:29:16 +0800

    * FIX [transport/mqtt] move one ref free of conn_param from protocol layer to transport pipe_close
    Signed-off-by: JaylinYu <letrangerjaylin@gmail.com>

--- src/sp/transport/mqtts/broker_tls.c
+++ src/sp/transport/mqtts/broker_tls.c
@@ -154,6 +154,7 @@ tlstran_pipe_close(void *arg)
 		p->npipe->subinfol = NULL;
 	}
 	nni_lmq_flush(&p->rslmq);
+	conn_param_free(p->tcp_cparam);
 	nni_mtx_unlock(&p->mtx);
 
 	nng_stream_close(p->conn);
