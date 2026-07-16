fc25fb7819f9c65516a6b1ade6d0738be6faca63
wangha <wanghamax@gmail.com>
2026-07-16 02:32:25 -0400

    * NEW [conf] Add ex_lmq for cache smsgs.
    Signed-off-by: wangha <wanghamax@gmail.com>

--- include/nng/supplemental/nanolib/conf.h
+++ include/nng/supplemental/nanolib/conf.h
@@ -656,7 +656,9 @@ struct conf_web_hook {
 
 	nng_mtx  *ex_mtx; // mutex for saios
 	nng_aio  *ex_aio; // Await flush
+	nng_aio  *ex_midaio; // Forward flush reqs in ex_aio cb
 	nng_aio **saios;  // Aios for sending message
+	nng_lmq  *ex_lmq; // Lmq for await flush
 
 	// TODO not support yet
 	conf_tls tls;
