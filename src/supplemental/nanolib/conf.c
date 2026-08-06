6bbec8b4878db23847559b00134712fd3e567b3d
Alvin <alvin@emqx.io>
2026-08-06 10:10:06 +0800

    * MDF [parquet] initialize name in conf_parquet and update topic match logic
    Signed-off-by: Alvin <alvin@emqx.io>

--- src/supplemental/nanolib/conf.c
+++ src/supplemental/nanolib/conf.c
@@ -3162,6 +3162,62 @@ conf_bridge_init(conf_bridge *bridge)
 	conf_sqlite_init(&bridge->sqlite);
 }
 
+static void
+conf_nng_proxy_init(conf_nng_bridge *proxy)
+{
+	proxy->pub_enable = false;
+	proxy->sub_enable = false;
+	proxy->pnodes = NULL;
+	proxy->snodes = NULL;
+
+	proxy->pub_count = 0;
+	proxy->sub_count = 0;
+}
+#if defined(SUPP_PARQUET) || defined(SUPP_LICENSE_STD)
+void
+conf_parquet_init(conf_parquet *parquet)
+{
+	parquet->enable                = false;
+	parquet->encryption.enable     = false;
+	parquet->encryption.key        = NULL;
+	parquet->encryption.key_id     = NULL;
+	parquet->encryption.type       = AES_GCM_V1;
+	parquet->encryption.key_cipher = NULL;
+
+	parquet->limit_frequency  = 5;
+	parquet->file_count       = 5;
+	parquet->file_size        = (10240 * 1024);
+	parquet->comp_type        = UNCOMPRESSED;
+	parquet->file_name_prefix = NULL;
+	parquet->dir              = NULL;
+	parquet->name             = NULL;
+}
+#endif
+
+void
+conf_bridge_pnode_init(conf_nng_pub_node *node)
+{
+	node->name = NULL;
+	node->pub_sock = (nng_socket) NNG_SOCKET_INITIALIZER;
+	node->pub_url = NULL;
+	node->clientid = NULL;
+	node->cparam = NULL;
+	node->pub_list = NULL;
+	node->forwards_count = 0;
+}
+
+void
+conf_bridge_snode_init(conf_nng_sub_node *node)
+{
+	node->name = NULL;
+	node->sub_sock = (nng_socket) NNG_SOCKET_INITIALIZER;
+	node->sub_url = NULL;
+	node->clientid = NULL;
+	node->cparam = NULL;
+	node->sub_list = NULL;
+	node->inwards_count = 0;
+}
+
 void
 conf_bridge_node_init(conf_bridge_node *node)
 {
