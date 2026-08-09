eeb9cd197d4b966a0a671ca194b0a4fbdc761a1e
JaylinYu <letrangerjaylin@gmail.com>
2026-08-09 09:10:00 +0800

    * FIX [transport] remove buggy conn_param_free from pipe_close
    Signed-off-by: JaylinYu <letrangerjaylin@gmail.com>

--- src/sp/transport/mqtts/broker_tls.c
+++ src/sp/transport/mqtts/broker_tls.c
@@ -150,7 +150,6 @@ tlstran_pipe_close(void *arg)
 		nni_free(p->npipe->subinfol, sizeof(nni_list));
 		p->npipe->subinfol = NULL;
 	}
-	conn_param_free(p->tcp_cparam);
 	nni_mtx_unlock(&p->mtx);
 
 	nng_stream_close(p->conn);
