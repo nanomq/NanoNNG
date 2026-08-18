4b1855a282c22d2b309bb802de5530cf65627aca
JaylinYu <letrangerjaylin@gmail.com>
2026-08-18 12:15:18 +0800

    * MDF [base64] prevent size overflow
    Signed-off-by: JaylinYu <letrangerjaylin@gmail.com>

--- include/nng/supplemental/nanolib/base64.h
+++ include/nng/supplemental/nanolib/base64.h
@@ -2,13 +2,13 @@
 #define BASE64_H
 #include "nng/nng.h"
 
-#define BASE64_ENCODE_OUT_SIZE(s) ((unsigned int)((((s) + 2) / 3) * 4 + 1))
-#define BASE64_DECODE_OUT_SIZE(s) ((unsigned int)(((s) / 4) * 3))
+#define BASE64_ENCODE_OUT_SIZE(s)                                           \
+    (((uint64_t)(s) > (((uint64_t)SIZE_MAX - 1) / 4) * 3 - 2)               \
+        ? 0                                                                 \
+        : (size_t) ((((((uint64_t)(s)) + 2) / 3) * 4) + 1))
 
-/*
- * out is null-terminated encode string.
- * return values is out length, exclusive terminating `\0'
- */
+#define BASE64_DECODE_OUT_SIZE(s)                                           \
+    ((size_t) (((((uint64_t)(s))) / 4) * 3))
 NNG_DECL unsigned int
 base64_encode(const unsigned char *in, unsigned int inlen, char *out);
 
