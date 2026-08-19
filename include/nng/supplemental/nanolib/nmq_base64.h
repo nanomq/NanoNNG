38701e41bce43e217f5f9d9d4fe57b8857df852f
JaylinYu <letrangerjaylin@gmail.com>
2026-08-19 12:05:00 +0800

    * FIX [scram/Base64] sweep old base63_encode/decode api caller
    Signed-off-by: JaylinYu <letrangerjaylin@gmail.com>

--- include/nng/supplemental/nanolib/nmq_base64.h
+++ include/nng/supplemental/nanolib/nmq_base64.h
@@ -9,11 +9,18 @@
 
  #define BASE64_DECODE_OUT_SIZE(s)                                           \
     ((size_t) ((((((uint64_t)(s)) + 3)) / 4) * 3))
+
+/*
+ * Encodes in_len bytes into a NUL terminated base64 string.
+ * Returns the number of characters written, excluding the NUL,
+ * or (size_t)-1 if out_len is too small.
+ */
 NNG_DECL size_t
 nmq_base64_encode(const uint8_t *in, size_t in_len, char *out, size_t out_len);
 
 /*
- * return values is out length
+ * Decodes in_len base64 characters.
+ * Returns the number of bytes written, or (size_t)-1 if out_len is too small.
  */
 NNG_DECL size_t
 nmq_base64_decode(const char *in, size_t in_len, uint8_t *out, size_t out_len);
