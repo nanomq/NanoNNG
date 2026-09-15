b7b40c59dce8ecfc572d122a1367ff6479e3c1b4
JaylinYu <letrangerjaylin@gmail.com>
2026-09-15 17:21:36 +0800

    * MDF [mqtt_codec] take review suggestions from AI
    Signed-off-by: JaylinYu <letrangerjaylin@gmail.com>

--- src/mqtt/protocol/mqtt/mqtt_client.c
+++ src/mqtt/protocol/mqtt/mqtt_client.c
@@ -1130,7 +1130,9 @@ mqtt_recv_cb(void *arg)
 			return;
 		}
 	} else if (s->mqtt_ver == MQTT_PROTOCOL_VERSION_v5) {
-		nni_msg_set_cmd_type(msg, CMD_PUBLISH_V5_RECV);
+		if ((*(uint8_t *) nni_msg_header(msg) & 0xF0) == CMD_PUBLISH) {
+			nni_msg_set_cmd_type(msg, CMD_PUBLISH_V5_RECV);
+		}
 		rv = nni_mqttv5_msg_decode(msg);
 		if (rv != MQTT_SUCCESS) {
 			// Msg should be clear if decode failed. We reuse it to send disconnect.
